87ac629c70973bcdff6bedd66f08ba04085b57d6
Lei Yang <leiy@csail.mit.edu>
2018-12-16 14:38:24 +0000

    auto init lnd

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 python3 bootstrap.py
 btcd &> /dev/null &
-lnd &> /dev/null &
+btcd_pid=$!
+lnd --noseedbackup &> /dev/null &
 etcd --config-file ~/.etcd/etcd.conf &> /dev/null &
 
 # wait for etcd to start
