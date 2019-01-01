96dbda413896ae580209d610ebd87e5328dc248e
Lei Yang <lei@yangl1996.com>
2019-01-01 18:22:40 -0500

    default tx size to 10000

--- setupexp.sh
+++ setupexp.sh
@@ -16,7 +16,7 @@ function next_index()
 function start_container()
 {
 	# name, ip, host
-	ssh $3 -- docker run -itd --name "spider$1" -e NODENAME=$1 -e NODEIP=$2 -e SPIDER_EXP_NAME='hotnets-02' -e SPIDER_QUEUE=0 -e EXP_TIME=300 --network spider --ip $2 spider
+	ssh $3 -- docker run -itd --name "spider$1" -e NODENAME=$1 -e NODEIP=$2 -e SPIDER_EXP_NAME='hotnets-02' --network spider --ip $2 spider
 }
 
 function destroy_container()
