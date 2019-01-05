a42813b6ebca4205d34dafc96026d8a1c96dfb16
Lei Yang <lei@yangl1996.com>
2019-01-04 21:07:56 -0500

    fix topology path

--- tools/run.sh
+++ tools/run.sh
@@ -112,7 +112,7 @@ function start_container
 {
     # $1: node name, $2: ip, $3: host
     topo_filename=`basename $TOPO_FILE`
-    topo_path="/root/ubuntu/topology/$topo_filename"
+    topo_path="/root/topology/$topo_filename"
     ssh $3 -- docker run -itd --name "spider$1" -e NODENAME=$1 -e NODEIP=$2 -e SPIDER_EXP_NAME="$EXP_NAME" -e TOPO_FILE="$topo_path" -e EXP_TIME="$EXP_TIME" --network spider --ip $2 spider
 }
 
