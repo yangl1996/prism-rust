d471249298c1e99f94e0783647b2463c61909453
Lei Yang <lei@yangl1996.com>
2019-01-06 16:28:49 -0500

    update topology files

---
+++ tools/fix_json.py
@@ -0,0 +1,35 @@
+#!/usr/local/bin/python3
+
+import sys
+import json 
+
+if len(sys.argv) != 2:
+    print("Usage: ./fix_json.py <file to fix>")
+    exit(1)
+
+filepath = sys.argv[1]
+print("Opening " + filepath)
+
+with open(filepath) as f:
+    dt = json.load(f)
+
+miner = dt['miner']
+print(miner)
+
+btc_conn = []
+for node in dt['nodes']:
+    if node['name'] == miner:
+        continue
+    btc_conn.append({'src': miner, 'dst': node['name']})
+dt['btcd_connections'] = btc_conn
+
+for ch in dt['lnd_channels']:
+    if ch['capacity'] == 1000000000:
+        ch['capacity'] = 100000000
+    elif ch['capacity'] == 50:
+        ch['capacity'] = 10000
+    else:
+        print("Invalid channel capacity")
+
+with open(filepath, 'w') as f:
+    json.dump(dt, f, indent=4)
