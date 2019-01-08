6348b00b8f44ae4ce57cf1a599fe288f18226bc6
Lei Yang <lei@yangl1996.com>
2019-01-08 15:13:52 -0500

    use new logging branch

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ export BUILDROOT="$PWD"
 export BTCPATH="$PWD/btcroot"
 export GOPATH="$PWD/goroot"
 export PATH="$PATH:$GOPATH/bin"
-export BRANCH='stats'
+export BRANCH='new-stats'
 
 BUILD_LND='false'
 BUILD_EXPCTRL='false'
