ee134ee28c90ff381033947d1e60c3f2cff30633
Lei Yang <lei@yangl1996.com>
2019-01-31 21:57:35 -0500

    add tests for block

--- src/block.rs
+++ src/block.rs
@@ -22,8 +22,11 @@ impl std::fmt::Display for Block {
 
 impl Block {
     pub fn serialized(&self) -> [u8; 36] {
+        // little-endian parent hash + little-endian nonce
         let mut serialized: [u8; 36] = [0; 36];
-        serialized[..32].clone_from_slice(&self.parent.0);
+        for idx in 0..32 {
+            serialized[idx] = self.parent.0[31-idx];
+        }
         LittleEndian::write_u32(&mut serialized[32..36], self.nonce);
         return serialized;
     }
@@ -40,11 +43,11 @@ impl Block {
 }
 
 #[derive(Eq)]
-pub struct BlockHash(pub [u8; 32]); // little endian u256
+pub struct BlockHash(pub [u8; 32]); // big endian u256
 
 impl Ord for BlockHash {
     fn cmp(&self, other: &BlockHash) -> std::cmp::Ordering {
-        for byte_idx in (1..32).rev() {
+        for byte_idx in 0..31 {
             let res = self.0[byte_idx].cmp(&other.0[byte_idx]);
             match res {
                 std::cmp::Ordering::Equal => {
@@ -55,7 +58,7 @@ impl Ord for BlockHash {
                 },
             }
         }
-        return self.0[0].cmp(&other.0[0]);
+        return self.0[31].cmp(&other.0[31]);
     }
 }
 
@@ -67,7 +70,7 @@ impl PartialOrd for BlockHash {
 
 impl PartialEq for BlockHash {
     fn eq(&self, other: &BlockHash) -> bool {
-        for byte_idx in (0..32).rev() {
+        for byte_idx in 0..32 {
             if self.0[byte_idx] != other.0[byte_idx] {
                 return false;
             }
@@ -78,7 +81,7 @@ impl PartialEq for BlockHash {
 
 impl std::fmt::Display for BlockHash {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        for byte_idx in (0..32).rev() {
+        for byte_idx in 0..32 {
             write!(f, "{:>02x}", self.0[byte_idx])?;
         }
         Ok(())
@@ -87,3 +90,76 @@ impl std::fmt::Display for BlockHash {
 
 //pub struct Transaction;
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn blockhash_ordering() {
+        let bigger_blockhash = BlockHash([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                         0, 0, 0, 0, 0, 0, 0, 1]);
+        let smaller_blockhash = BlockHash([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(bigger_blockhash > smaller_blockhash, true);
+
+        let bigger_blockhash = BlockHash([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                         0, 0, 0, 0, 0, 0, 0, 0]);
+        let smaller_blockhash = BlockHash([0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(bigger_blockhash > smaller_blockhash, true);
+
+        let some_blockhash = BlockHash([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                         0, 0, 0, 0, 0, 0, 0, 0]);
+        let same_blockhash = BlockHash([0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                                          0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(some_blockhash >= same_blockhash, true);
+        assert_eq!(some_blockhash <= same_blockhash, true);
+        assert_eq!(some_blockhash == same_blockhash, true);
+    }
+
+    #[test]
+    fn block_serialization() {
+        let block = Block {
+            parent: BlockHash([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                               10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                               10, 10, 10, 10, 10, 10, 10, 10, 10, 9]),
+            nonce: 12345,
+        };
+        let serialized = block.serialized();
+        let should_be: [u8; 36] = [9, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                                   10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                                   10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 57,
+                                   48, 0,  0];
+        let mut comp = true;
+        for idx in 0..36 {
+            print!("| {}, {} |", serialized[idx], should_be[idx]);
+            if serialized[idx] != should_be[idx] {
+                comp = false;
+            }
+        }
+        assert_eq!(comp, true);
+    }
+
+    #[test]
+    fn block_hash() {
+        let block = Block {
+            parent: BlockHash([10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                               10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10,
+                               10, 10, 10, 10, 10, 10, 10, 10, 10, 9]),
+            nonce: 12345,
+        };
+        let hash = block.hash();
+        let should_be = BlockHash([0x58, 0x87, 0xa8, 0xbc, 0xba, 0xde, 0x11,
+                                   0x0e, 0x4b, 0xa7, 0x8f, 0x95, 0x01, 0xe6,
+                                   0x39, 0xd0, 0x5d, 0x1f, 0xf9, 0xdf, 0xd6,
+                                   0xab, 0xd2, 0x1f, 0x93, 0x69, 0x56, 0xa8,
+                                   0x08, 0xab, 0xbb, 0xfb]);
+        assert_eq!(hash == should_be, true);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,3 @@
-mod block;
-
 fn main() {
-    let block = block::Block {
-        parent: block::BlockHash([10; 32]),
-        nonce: 12345,
-    };
-    println!("Block: {}", block);
-    let serialized = block.serialized();
-    print!("Serialized: ");
-    for i in 0..36 {
-        print!("{:>02x}", serialized[i]);
-    }
-    println!("");
-    let hash = block.hash();
-    println!("Hash: {}", hash);
+    println!("I'm just a placeholder");
 }
