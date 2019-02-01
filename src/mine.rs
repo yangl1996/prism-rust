1fc40e117133bc184bd1db67c266c3d34fd81759
Lei Yang <lei@yangl1996.com>
2019-01-31 21:57:35 -0500

    add tests for block

--- src/mine.rs
+++ src/mine.rs
@@ -5,6 +5,11 @@ use block::{Block, BlockHash};
 pub fn mine(&mut block::Block block, &block::BlockHash thld) {
     for nonce in 0..std::u32::MAX {
         block.nonce = nonce;
-        
+        let hash = block.hash();
+        if hash < thld {
+            return;
+        }
     }
+    // TODO: we should not arrive here
+    return;
 }
