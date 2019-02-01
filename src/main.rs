1b29b4e473975665cce49e582e2d3083d4817430
Lei Yang <lei@yangl1996.com>
2019-01-31 21:06:19 -0500

    add hash for block

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,13 @@ fn main() {
         parent: block::BlockHash([10; 32]),
         nonce: 12345,
     };
-    println!("{}", block);
+    println!("Block: {}", block);
     let serialized = block.serialized();
+    print!("Serialized: ");
     for i in 0..36 {
         print!("{:>02x}", serialized[i]);
     }
+    println!("");
+    let hash = block.hash();
+    println!("Hash: {}", hash);
 }
