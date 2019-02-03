9a8bdd42287842e58f947447e136a58f7a83e66a
Lei Yang <lei@yangl1996.com>
2019-02-03 09:43:03 -0500

    add block header module

--- src/block/block_header.rs
+++ src/block/block_header.rs
@@ -1,69 +1,55 @@
 extern crate bincode;
 
-use super::block_hash::BlockHash;
-
-const NUM_TXN_BLOCKS: usize = 4; // how many txn blocks in a core block
+use super::hash;
 
 #[derive(Serialize, Deserialize)]
-pub struct CoreBlock {
-    pub parent: BlockHash,
-    pub transaction_blocks: [BlockHash; NUM_TXN_BLOCKS],
+pub struct BlockHeader {
+    pub voter_hash: hash::Hash,
+    pub proposal_hash: hash::Hash,
+    pub transactions_hash: hash::Hash,
     pub nonce: u32,
 }
 
-impl std::fmt::Display for CoreBlock {
+impl std::fmt::Display for BlockHeader {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "{{\n")?;
-        //write!(f, "  transactions: not implemented")?;
-        write!(f, "  parent: {}\n", self.parent)?;
-        write!(f, "  transaction blocks: {{\n")?;
-        for txn in &self.transaction_blocks {
-            write!(f, "    {},\n", txn)?;
-        }
-        write!(f, "  }}\n")?;
+        write!(f, "  voter hash: {}\n", self.voter_hash)?;
+        write!(f, "  proposal hash: {}\n", self.proposal_hash)?;
+        write!(f, "  transactions hash: {}\n", self.transactions_hash)?;
         write!(f, "  nonce: {}\n", self.nonce)?;
         write!(f, "}}")
     }
 }
 
-impl super::Block for CoreBlock {
-    fn serialize(&self) -> Vec<u8> {
-        return bincode::serialize(&self).unwrap();
-    }
-
-    fn hash(&self) -> BlockHash {
-        // TODO: we don't specifically arrange the bytes in the Block
-        // struct, so the hash depends on how ring serializes the bytes
-        let serialized = self.serialize();
+impl hash::Hashable for BlockHeader {
+    fn hash(&self) -> hash::Hash {
+        let serialized = bincode::serialize(&self).unwrap();
         let digest = ring::digest::digest(&ring::digest::SHA256, &serialized);
         let mut raw_hash: [u8; 32] = [0; 32];
         raw_hash[..32].clone_from_slice(digest.as_ref());
-        return BlockHash(raw_hash);
+        return raw_hash.into();
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use crate::block::Block;
+    use super::super::hash;
+    use super::super::hash::Hashable;
+    use super::BlockHeader;
 
     #[test]
-    fn block_hash() {
-        let block = CoreBlock {
-            parent: BlockHash([5; 32]),
-            transaction_blocks: [
-                BlockHash([1; 32]),
-                BlockHash([2; 32]),
-                BlockHash([4; 32]),
-                BlockHash([3; 32]),
-            ],
+    fn hash() {
+        let block_header = BlockHeader {
+            voter_hash: hash::Hash([1; 32]),
+            proposal_hash: hash::Hash([2; 32]),
+            transactions_hash: hash::Hash([3; 32]),
             nonce: 12345,
         };
-        let hash = block.hash();
-        let should_be = BlockHash([
-            0xa3, 0x90, 0x46, 0xd3, 0xaf, 0xfa, 0x8b, 0x05, 0xe6, 0x20, 0x80, 0xe2, 0x67, 0x21,
-            0x92, 0xef, 0x04, 0x7a, 0x15, 0xf9, 0xd7, 0x81, 0x84, 0xcb, 0x0b, 0x0c, 0x0d, 0x30,
-            0xdf, 0x8f, 0x8e, 0x55,
+        let hash = block_header.hash();
+        let should_be = hash::Hash([
+            0x29, 0xe6, 0x70, 0x3a, 0x08, 0x0f, 0x12, 0x2e, 0x9a, 0xc4, 0x55, 0xae, 0xdf, 0xbe,
+            0x9b, 0xd1, 0x97, 0x44, 0x92, 0xdf, 0x74, 0xf8, 0x8a, 0xd9, 0x70, 0xc0, 0x7b, 0x82,
+            0x4d, 0x4e, 0xa2, 0x92,
         ]);
         assert_eq!(hash == should_be, true);
     }
