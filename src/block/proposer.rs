ee1dc9fe458eed025ad4291ab371aec9a593ffba
Lei Yang <lei@yangl1996.com>
2019-02-05 10:24:45 -0500

    cargo fmt

--- src/block/proposer.rs
+++ src/block/proposer.rs
@@ -1,11 +1,11 @@
 extern crate bincode;
 extern crate ring;
 
-use super::hash;
 use super::block_header;
+use super::hash;
+use super::hash::Hashable;
 use super::transaction;
 use super::Block;
-use super::hash::Hashable;
 
 pub struct ProposerBlock {
     pub header: block_header::BlockHeader,
@@ -59,12 +59,12 @@ impl hash::Hashable for ProposerMetadata {
 
 #[cfg(test)]
 mod tests {
+    use super::super::block_header;
     use super::super::hash;
     use super::super::hash::Hashable;
     use super::super::Block;
-    use super::super::block_header;
-    use super::ProposerMetadata;
     use super::ProposerBlock;
+    use super::ProposerMetadata;
 
     macro_rules! fake_proposer {
         () => {
@@ -77,7 +77,9 @@ mod tests {
                 },
                 transactions: vec![],
                 metadata: ProposerMetadata {
-                    level_cert: hash::Hash(hex!("0102030405060708010203040506070801020304050607080102030405060708")),
+                    level_cert: hash::Hash(hex!(
+                        "0102030405060708010203040506070801020304050607080102030405060708"
+                    )),
                     ref_links: vec![],
                 },
             }
@@ -88,27 +90,32 @@ mod tests {
     fn metadata_hash() {
         let metadata = ProposerMetadata {
             level_cert: hash::Hash(hex!(
-                                "0102030401020304010203040102030401020304010203040102030401020304"
-                                )),
-                                ref_links: vec![
-                                    hash::Hash(hex!(
-                                            "0102030405060504010203040506050401020304050605040102030405060504"
-                                            )),
-                                            hash::Hash(hex!(
-                                                    "0403020104030201040302010403020104030201040302010403020104030201"
-                                                    )),
-                                ],
+                "0102030401020304010203040102030401020304010203040102030401020304"
+            )),
+            ref_links: vec![
+                hash::Hash(hex!(
+                    "0102030405060504010203040506050401020304050605040102030405060504"
+                )),
+                hash::Hash(hex!(
+                    "0403020104030201040302010403020104030201040302010403020104030201"
+                )),
+            ],
         };
         let hash = metadata.hash();
         let should_be = hash::Hash(hex!(
-                "4062181720a6bf68005ce3f421566d725af5ca2b58175e305536f74be44ee71d"
-                ));
+            "4062181720a6bf68005ce3f421566d725af5ca2b58175e305536f74be44ee71d"
+        ));
         assert_eq!(hash, should_be);
     }
 
     #[test]
     fn block_hash() {
         let block = fake_proposer!();
-        assert_eq!(block.hash(), hash::Hash(hex!("29e6703a080f122e9ac455aedfbe9bd1974492df74f88ad970c07b824d4ea292")));
+        assert_eq!(
+            block.hash(),
+            hash::Hash(hex!(
+                "29e6703a080f122e9ac455aedfbe9bd1974492df74f88ad970c07b824d4ea292"
+            ))
+        );
     }
 }
