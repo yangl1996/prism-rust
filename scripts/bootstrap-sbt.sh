37f1d1669c7b5b754ef9d9867522dac3f88a40ad
Lei Yang <lei@yangl1996.com>
2019-02-14 14:45:34 -0500

    install async-profiler

--- scripts/bootstrap-sbt.sh
+++ scripts/bootstrap-sbt.sh
@@ -15,6 +15,14 @@ sudo mv /home/ubuntu/download/jvmtop/jvmtop.jar /usr/local/bin
 sudo mv /home/ubuntu/download/jvmtop/jvmtop.sh /usr/local/bin/jvmtop
 sudo chmod +x /usr/local/bin/jvmtop
 
+echo "Installing async-profiler"
+wget 'https://github.com/jvm-profiling-tools/async-profiler/releases/download/v1.5/async-profiler-1.5-linux-x64.tar.gz' -O /home/ubuntu/download/async-profiler.tar.gz &>/dev/null
+mkdir -p /home/ubuntu/download/async-profiler
+tar xf /home/ubuntu/download/async-profiler.tar.gz -C /home/ubuntu/download/async-profiler &>/dev/null
+sudo mv /home/ubuntu/download/async-profiler/build /usr/local/bin
+sudo mv /home/ubuntu/download/async-profiler/profiler.sh /usr/local/bin/profiler
+sudo chmod +x /usr/local/bin/profiler
+
 echo "Adding sbt repository"
 echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
