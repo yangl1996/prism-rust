069271745f22a325b222ede24b24d875855a7159
Lei Yang <lei@yangl1996.com>
2019-02-21 12:03:09 -0500

    add root for merkle tree

--- src/merkle.rs
+++ src/merkle.rs
@@ -31,8 +31,8 @@ impl<'a, T: Hashable> MerkleTree<'a, T> {
     fn new(data: &'a [T]) -> Self {
         // calculate the size of the tree
         let mut this_layer_size = data.len();
-        let mut layer_size = vec![];    // size after dup
-        let mut data_size = vec![];     // size before dup
+        let mut layer_size = vec![]; // size after dup
+        let mut data_size = vec![]; // size before dup
         loop {
             data_size.push(this_layer_size);
             if this_layer_size == 1 {
@@ -77,13 +77,17 @@ impl<'a, T: Hashable> MerkleTree<'a, T> {
             }
             if l != d {
                 nodes[layer_start + l - 1] = nodes[layer_start + d - 1];
-            } 
+            }
         }
 
         return MerkleTree {
             data: data,
             nodes: nodes,
         };
+    }
+
+    fn root(&self) -> &hash::SHA256 {
+        return &self.nodes[0];
     }
 }
 
@@ -129,6 +133,41 @@ mod tests {
             merkle_tree.nodes[13],
             hash::SHA256(hex!(
                 "b8027a4fc86778e60f636c12e67d03b7356f1d6d8a8ff486bcdaa3dcf81b714b"
+            ))
+        );
+    }
+
+    #[test]
+    fn root() {
+        let input_data = vec![
+            hash::SHA256(hex!(
+                "0a0b0c0d0e0f0e0d0a0b0c0d0e0f0e0d0a0b0c0d0e0f0e0d0a0b0c0d0e0f0e0d"
+            )),
+            hash::SHA256(hex!(
+                "0102010201020102010201020102010201020102010201020102010201020102"
+            )),
+            hash::SHA256(hex!(
+                "0a0a0a0a0b0b0b0b0a0a0a0a0b0b0b0b0a0a0a0a0b0b0b0b0a0a0a0a0b0b0b0b"
+            )),
+            hash::SHA256(hex!(
+                "0403020108070605040302010807060504030201080706050403020108070605"
+            )),
+            hash::SHA256(hex!(
+                "1a2a3a4a1a2a3a4a1a2a3a4a1a2a3a4a1a2a3a4a1a2a3a4a1a2a3a4a1a2a3a4a"
+            )),
+            hash::SHA256(hex!(
+                "deadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeefdeadbeef"
+            )),
+            hash::SHA256(hex!(
+                "0000000100000001000000010000000100000001000000010000000100000001"
+            )),
+        ];
+        let merkle_tree = MerkleTree::new(&input_data);
+        let root = merkle_tree.root();
+        assert_eq!(
+            root,
+            &hash::SHA256(hex!(
+                "9d8f0638fa3d46f618dea970df55b53a02f4aa924e8d598af6b5f296fdaabce5"
             ))
         );
     }
