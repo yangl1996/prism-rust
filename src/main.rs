69e3a5b78ca1a19b3b96465707287de67f90558d
Lei Yang <lei@yangl1996.com>
2019-02-28 11:06:31 -0500

    better error handling

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ extern crate hex_literal;
 #[macro_use]
 extern crate clap;
 
-use log::{debug, info, warn, error};
-use std::process;
+use log::{debug, error, info, warn};
 use std::net;
+use std::process;
 
 mod hash;
 mod merkle;
@@ -19,11 +19,11 @@ const DEFAULT_P2P_PORT: u16 = 6000;
 fn main() {
     // parse command line arguments
     let matches = clap_app!(Prism =>
-    (version: "0.1")
-    (about: "Prism blockchain full client")
-    (@arg verbose: -v ... "Increases the verbosity of logging")
-    (@arg peer_ip: --ip [IP] "Sets the IP address to listen to peers")
-    (@arg peer_port: --port [PORT] "Sets the port to listen to peers")
+     (version: "0.1")
+     (about: "Prism blockchain full client")
+     (@arg verbose: -v ... "Increases the verbosity of logging")
+     (@arg peer_ip: --ip [IP] "Sets the IP address to listen to peers")
+     (@arg peer_port: --port [PORT] "Sets the port to listen to peers")
     )
     .get_matches();
 
@@ -33,33 +33,24 @@ fn main() {
 
     // start p2p server
     let peer_ip = match matches.value_of("peer_ip") {
-        Some(ip) => {
-            match ip.parse::<net::IpAddr>() {
-                Ok(parsed) => parsed,
-                Err(e) => {
-                    error!("Error parsing IP address: {}", e);
-                    process::exit(1);
-                },
-            }
-        },
+        Some(ip) => ip.parse::<net::IpAddr>().unwrap_or_else(|e| {
+            error!("Error parsing P2P IP address: {}", e);
+            process::exit(1);
+        }),
         None => DEFAULT_IP.parse::<net::IpAddr>().unwrap(),
     };
     let peer_port = match matches.value_of("peer_port") {
-        Some(port) => {
-            match port.parse::<u16>() {
-                Ok(parsed) => parsed,
-                Err(e) => {
-                    error!("Error parsing p2p port: {}", e);
-                    process::exit(1);
-                },
-            }
-        },
+        Some(port) => port.parse::<u16>().unwrap_or_else(|e| {
+            error!("Error parsing P2P port: {}", e);
+            process::exit(1);
+        }),
         None => DEFAULT_P2P_PORT,
     };
     let peer_socket_addr = net::SocketAddr::new(peer_ip, peer_port);
-    debug!("Starting P2P server at {}", peer_socket_addr);
 
-    network::server::p2p_server(peer_socket_addr);
-    loop {
-    }
+    debug!("Starting P2P server at {}", peer_socket_addr);
+    network::server::p2p_server(peer_socket_addr).map_err(|e| {
+        error!("Error starting P2P server: {}", e);
+        process::exit(1);
+    });
 }
