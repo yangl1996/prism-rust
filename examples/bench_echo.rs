9071873d8e63ba78a02685287e32007d6358bef5
Lei Yang <lei@yangl1996.com>
2019-03-05 18:30:06 -0500

    better server api

--- examples/bench_echo.rs
+++ examples/bench_echo.rs
@@ -1,18 +1,14 @@
 use byteorder::{BigEndian, ByteOrder};
 use std::io::Write;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::thread;
-use std::time::{Duration, Instant};
+use std::time::Instant;
 
-const MSG_SIZE: usize = 1024;
+const MSG_SIZE: usize = 10240;
 const REPEAT_TIME: usize = 100000;
 
 fn main() {
     let listen_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9999);
-    let mut server = prism::network::server::Server::new(listen_addr).unwrap();
-    thread::spawn(move || {
-        server.listen().unwrap();
-    });
+    let server = prism::network::server::Server::start(listen_addr).unwrap();
 
     let message = prism::network::message::Message::EchoRequest("a".repeat(MSG_SIZE));
     let encoded: Vec<u8> = bincode::serialize(&message).unwrap();
@@ -24,11 +20,11 @@ fn main() {
     let mut writer = std::io::BufWriter::new(socket);
 
     let start = Instant::now();
-    for i in 0..REPEAT_TIME {
+    for _ in 0..REPEAT_TIME {
         writer.write(&length_encoded).unwrap();
         writer.write(&encoded).unwrap();
     }
-    writer.flush();
+    writer.flush().unwrap();
     let end = Instant::now();
     let time = end.duration_since(start).as_micros() as f64;
     let throughput = MSG_SIZE as f64 * REPEAT_TIME as f64 * 1000000.0 / time / 1024.0 / 1024.0;
