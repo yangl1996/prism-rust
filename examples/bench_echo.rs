a9b411456177786e980ecafbf16d5c4b341fd552
Lei Yang <lei@yangl1996.com>
2019-03-05 17:19:39 -0500

    server is now thread safe

--- examples/bench_echo.rs
+++ examples/bench_echo.rs
@@ -1,19 +1,19 @@
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use std::thread;
 use byteorder::{BigEndian, ByteOrder};
 use std::io::Write;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::thread;
 use std::time::{Duration, Instant};
 
-const MSG_SIZE: usize = 1024 * 1024 ;
+const MSG_SIZE: usize = 1024;
 const REPEAT_TIME: usize = 100000;
 
 fn main() {
     let listen_addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 9999);
     let mut server = prism::network::server::Server::new(listen_addr).unwrap();
-    thread::spawn(move|| {
+    thread::spawn(move || {
         server.listen().unwrap();
     });
-    
+
     let message = prism::network::message::Message::EchoRequest("a".repeat(MSG_SIZE));
     let encoded: Vec<u8> = bincode::serialize(&message).unwrap();
     let length: u32 = encoded.len() as u32;
@@ -33,5 +33,9 @@ fn main() {
     let time = end.duration_since(start).as_micros() as f64;
     let throughput = MSG_SIZE as f64 * REPEAT_TIME as f64 * 1000000.0 / time / 1024.0 / 1024.0;
     println!("Message size: {} KB", MSG_SIZE / 1024);
-    println!("Throughput: {:.3} MB/s, {:.2} messages/s", throughput, REPEAT_TIME as f64 * 1000000.0 / time);
+    println!(
+        "Throughput: {:.3} MB/s, {:.2} messages/s",
+        throughput,
+        REPEAT_TIME as f64 * 1000000.0 / time
+    );
 }
