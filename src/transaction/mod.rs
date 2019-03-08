45f5c29ff88f4a5f8cfa2ed9f1fa40ac856151b5
Lei Yang <lei@yangl1996.com>
2019-03-07 21:23:40 -0500

    reorganize transaction
    1. use similar class definition as bitcoin, but ignore the script
       language in bitcoin
    2. removed coin definitioin for now, as a coin is essentially a
       transaction output
    3. rename validators

--- src/transaction/mod.rs
+++ src/transaction/mod.rs
@@ -1,3 +1,56 @@
-pub mod transaction;
 pub mod coin;
-pub mod transaction_validators;
+pub mod validators;
+
+use crate::crypto::hash::{Hashable, H256};
+
+pub type PubKey = u8;//will be changed
+pub type Nonce = u32;
+pub type Signature = u8;
+pub type FromPair = (PubKey, Nonce);
+pub type ToPair = (PubKey, u32);
+
+
+/// A Prism transaction. A transaction takes a set of existing coins and transforms them into a set
+/// of output coins.
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Transaction {
+    input: Vec<Input>,
+    output: Vec<Output>,
+}
+
+impl Hashable for Transaction {
+    fn hash(&self) -> H256 {
+        unimplemented!();
+    }
+}
+
+/// An input of a transaction.
+struct Input {
+    /// A "pointer" to the coin being used.
+    previous_output: OutPoint,
+    /// The public key of the coin owner. We need this information because the transaction output
+    /// only contains the hash of the public key, but the whole public key is needed to verify the
+    /// signature.
+    pubkey: PubKey,
+    /// The signature by the coin owner. The coin owner signs this input using its private key to
+    /// prove that it really owns this coin.
+    signature: Signagure,
+}
+
+/// An output of a transaction.
+// TODO: coinbase output (transaction fee). Maybe we don't need that in this case.
+struct Output {
+    /// The amount of this output.
+    value: u64,
+    /// The hash of the public key of the recipient (a.k.a. blockchain address).
+    recipient: H256,
+}
+
+/// A "pointer" to a transaction output
+struct OutPoint {
+    /// The hash of the transaction being referred to.
+    hash: H256,
+    /// The index of the output in question in that transaction.
+    index: u32,
+}
+
