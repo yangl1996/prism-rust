a5a51404df3f717353dd49b878ea2fc5389bb896
Lei Yang <lei@yangl1996.com>
2019-03-10 18:53:12 -0400

    call message handler in read function

--- src/network/message.rs
+++ src/network/message.rs
@@ -1,5 +1,20 @@
+use log::{debug, error, info, trace, warn};
+use super::server::Peer;
+
 #[derive(Serialize, Deserialize, Debug)]
 pub enum Message {
-    EchoRequest(String),
-    EchoResponse(String),
+    Ping(String),
+    Pong(String),
+}
+
+/// Takes the reference to a message and processes it.
+pub fn handle_message(peer: &Peer, message: &Message) {
+    match message {
+        Message::Ping(nonce) => {
+            info!("Ping message from {}: {}", peer.addr, nonce);
+        },
+        Message::Pong(nonce) => {
+            info!("Pong message from {}: {}", peer.addr, nonce);
+        },
+    }
 }
