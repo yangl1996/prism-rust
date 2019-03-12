4bd5a921321482347a106882b2bb51b0e2c6ec58
Vivek Kumar Bagaria <vivekkumarbagaria@DNa1c4119.SUNet>
2019-03-12 00:53:18 -0700

    Added  comments on blockgraph. Created folder for validators

--- src/blockgraph/mod.rs
+++ src/blockgraph/mod.rs
@@ -49,18 +49,20 @@ impl<'a> BlockChainGraph<'a>{
         return self.voter_chains.len() as u32;
     }
 
-    /// Add a new block to the graph. Called when a new block is heard.
+    /// Add a new block to the graph. This function is called when a new block is received.
     pub fn add_block_as_node(&mut self, block: Block){
         let content: &Content = &block.content;
         match content {
             Content::Transaction(_c) => {
-                // todo: validate content
                 // todo: add transaction block to the unreferenced tx block pool (global)
+
+                /// Create a default tx node whose edges will be populated later
                 let mut tx_node = TxNode::default();
-                // Initializing node id
+                //// Initializing node id
                 tx_node.node_id = block.hash();
                 /// Extracting the reference of proposer block corresponding to the hash of the block's parent
                 let parent_prop_node :&PropNode = self.proposer_tree.get_prop_node_from_node_id(&block.header.parent_hash);
+                // Adding an (parent) directed edge from  tx_node to the parent_prop_node
                 tx_node.parent_prop_node = Some(parent_prop_node);
                 // The tx block has not been yet referenced so we dont set its child.
 //              /// Add the tx block to the tx block pool
@@ -68,7 +70,7 @@ impl<'a> BlockChainGraph<'a>{
             },
 
             Content::Proposer(c) => {
-                // todo: validate content
+                /// Create a default prop node whose edges will be populated later
                 let mut prop_node = PropNode::default();
                 // Initializing node id
                 prop_node.node_id = block.hash();
@@ -79,29 +81,26 @@ impl<'a> BlockChainGraph<'a>{
                 prop_node.level = parent_prop_node.level + 1;
 
 
-                /// Iterating through the list of transaction blocks referred in the content
+                /// Iterating through the list of transaction blocks referred in the content of the proposer block
                 for tx_block_hash in c.transaction_block_refs.iter(){
                     /// Extracting the reference of tx block corresponding to tx_block_hash
                     let tx_node_referred :&TxNode = self.tx_block_pool.get_tx_node_from_node_id(&tx_block_hash);
+                    /// Adding an (ref) directed edge from prop_node to the tx_node_referred
                     prop_node.add_tx_reference(tx_node_referred);
-                }
-
-                /// Iterating through the list of proposer blocks referred in the content
-                for prop_block_hash in c.proposer_block_refs.iter(){
-                    /// Extracting the reference of proposer block corresponding to prop_block_hash
-                    let prop_node_referred :&PropNode = self.proposer_tree.get_prop_node_from_node_id(&prop_block_hash);
-                    prop_node.add_referred_node(prop_node_referred);
 //                    prop_node_referred.add_child_node(&prop_node); // todo: Make it work
-
                 }
+//                /// Add the prop_node to proposer tree
 //                self.proposer_tree.add_node(prop_node); todo: Make this work. Lifetime issues
             },
 
             Content::Voter(c) => {
-                // todo: validate content
+                /// Create a default voter node whose edges will be populated later
                 let mut voter_node = VoterNode::default();
+                // Initialize chain id of the node
                 voter_node.chain_id = c.chain_id;
+                /// This node will be added tot the voter chain with chain_id c.chain_id
                 let voter_chain = &mut self.voter_chains[c.chain_id as usize];
+                // Initializing node id
                 voter_node.node_id = block.hash();
                 /// Extracting the reference of voter block corresponding to the hash of the block's voter parent
                 let chain_parent_node :&VoterNode = voter_chain.get_voter_node_from_node_id(&c.voter_parent_hash);
@@ -109,19 +108,20 @@ impl<'a> BlockChainGraph<'a>{
                 /// Extracting the reference of proposer block corresponding to the hash of the block's parent
                 let parent_prop_node :&PropNode = self.proposer_tree.get_prop_node_from_node_id(&block.header.parent_hash);
                 voter_node.parent_prop_node = Some(parent_prop_node);
+                // The level of a voter block  is  defined as 1 more than its parent's level.
                 voter_node.level = chain_parent_node.level + 1;
 
                 /// Iterating through the list of votes referred in the content
                 for votes in c.proposer_block_votes.iter(){
                     /// Extracting the reference of tx block corresponding to tx_block_hash
                     let prop_node_voted :&PropNode = self.proposer_tree.get_prop_node_from_node_id(&votes);
+                    /// Adding an (vote) directed edge from voter_node to the prop_node_voted
                     voter_node.add_vote(prop_node_voted);
 //                    prop_node_voted.add_vote(&voter_node); // todo: Make it work
-
                 }
+//                /// Add the voter_node to voter chain
 //                voter_chain.add_node(voter_node); // todo: Make this work. Lifetime issues
             },
         };
-
     }
 }
