ca76dc9e73c9d604f49d682cb706d3d08d2d5626
Vivek Kumar Bagaria <vivekee047@gmail.com>
2019-03-16 18:06:55 -0700

    Added random generator for h256, header

---
+++ src/block/generator.rs
@@ -0,0 +1,28 @@
+/*
+It randomly generates objects of the given class
+*/
+
+use super::{Block, Content};
+use super::header::Header;
+use super::transaction::Content as tx_Content;
+use super::proposer::Content as proposer_Content;
+use super::voter::Content as voter_Content;
+
+use crate::crypto::hash::{Hashable, H256};
+use crate::crypto::merkle::{MerkleTree};
+use crate::crypto::generator as crypto_generator;
+use crate::transaction::{Transaction,  Input, Output, Signature};
+use rand::{Rng, RngCore};
+type rgen = rand::prelude::ThreadRng;
+
+
+pub fn header() -> Header {
+    let mut rng = rand::thread_rng();
+    let parent_hash = crypto_generator::H256();
+    let timestamp = rng.next_u64();
+    let nonce = rng.next_u32();
+    let content_root = crypto_generator::H256();
+    let extra_content = crypto_generator::u8_32_array();
+    let difficulty = crypto_generator::u8_32_array();
+    return Header::new(parent_hash, timestamp, nonce, content_root, extra_content, difficulty)
+}
