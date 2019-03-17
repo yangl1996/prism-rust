062e2bcbfea41ad6e96cfa239a49b0e5bbe9cd42
Vivek Kumar Bagaria <vivekkumarbagaria@DNa1c4119.SUNet>
2019-03-16 21:16:32 -0700

    Added tx generator and tx_content generator

--- src/block/generator.rs
+++ src/block/generator.rs
@@ -11,6 +11,7 @@ use super::voter::Content as voter_Content;
 use crate::crypto::hash::{Hashable, H256};
 use crate::crypto::merkle::{MerkleTree};
 use crate::crypto::generator as crypto_generator;
+use crate::transaction::generator as tx_generator;
 use crate::transaction::{Transaction,  Input, Output, Signature};
 use rand::{Rng, RngCore};
 type rgen = rand::prelude::ThreadRng;
@@ -25,4 +26,11 @@ pub fn header() -> Header {
     let extra_content = crypto_generator::u8_32_array();
     let difficulty = crypto_generator::u8_32_array();
     return Header::new(parent_hash, timestamp, nonce, content_root, extra_content, difficulty)
+}
+
+pub fn tx_content()  -> tx_Content {
+    let mut rng = rand::thread_rng();
+    let tx_size =  rng.gen_range(1, 10);
+    let transactions :Vec<Transaction> = (0..tx_size).map(|_| tx_generator::transaction()).collect();
+    return tx_Content {transactions};
 }
