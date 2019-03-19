c98703413aaf4873cd503c27592fd30710419615
wgr523 <wgr523@gmail.com>
2019-03-19 18:32:38 -0500

    add more to transaction validator

--- src/validation/transaction.rs
+++ src/validation/transaction.rs
@@ -3,13 +3,13 @@ Validation for blocks and transactions.
 */
 use crate::transaction::Transaction;
 pub trait TransactionValidator {
-    fn is_valid(transaction: &Transaction) -> bool;
+    fn is_valid(&self, transaction: &Transaction) -> bool;
 }
 
 pub struct NonEmptyValidator;
 
 impl TransactionValidator for NonEmptyValidator {
-    fn is_valid(transaction: &Transaction) -> bool {
+    fn is_valid(&self, transaction: &Transaction) -> bool {
         !(transaction.input.is_empty() || transaction.input.is_empty())
     }
 }
@@ -17,9 +17,19 @@ impl TransactionValidator for NonEmptyValidator {
 pub struct SignatureValidator;
 
 impl TransactionValidator for SignatureValidator {
-    fn is_valid(transaction: &Transaction) -> bool {
+    fn is_valid(&self, transaction: &Transaction) -> bool {
         unimplemented!()
         //transaction.input.len() == transaction.signatures.len()
+    }
+}
+
+pub struct AllValidator {
+    list: Vec<Box<TransactionValidator>>,
+}
+
+impl TransactionValidator for AllValidator {
+    fn is_valid(&self, transaction: &Transaction) -> bool {
+        self.list.iter().all(|validator|validator.is_valid(transaction))//TODO question: do we use &validator?
     }
 }
 
@@ -28,9 +38,11 @@ impl TransactionValidator for SignatureValidator {
 pub mod tests {
     use super::{NonEmptyValidator, TransactionValidator};
     use crate::transaction::transaction_builder::TransactionBuilder;
+    use crate::validation::transaction::AllValidator;
 
     #[test]
-    fn test_nonemptyvalidator() {
-        assert!(NonEmptyValidator::is_valid(&TransactionBuilder::random_transaction_builder().into()));
+    fn test_allvalidator() {
+        let v = AllValidator { list:vec![Box::new(NonEmptyValidator{})]};
+        assert!(v.is_valid(&TransactionBuilder::random_transaction_builder().into()));
     }
 }
