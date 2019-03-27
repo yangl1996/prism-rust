2eafc5769bcb33fbe9b9e8760681fcc12e822c94
wgr523 <wgr523@gmail.com>
2019-03-27 18:46:04 -0500

    add safe and total coins to wallet

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -1,5 +1,5 @@
 use crate::crypto::hash::{H256, Hashable};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use crate::transaction::{Transaction, Input, Output};
 use crate::crypto::sign::PubKey;
 
@@ -7,6 +7,8 @@ use crate::crypto::sign::PubKey;
 pub struct Wallet {
     /// Transaction outpoint -> coin value and owner. (owner must be this user)
     by_outpoint: HashMap<Input, Output>,
+    /// Transaction outpoint that are used to create transaction, but not confirmed
+    by_used_outpoint: HashMap<Input, Output>,
     /// The (hashed) public keys of this user
     pubkey_hash: H256,// later should change to pubkeys: Vec<PubKey>,
 }
@@ -15,6 +17,7 @@ impl Wallet {
     pub fn new(pubkey_hash: H256) -> Self {
         return Self {
             by_outpoint: HashMap::new(),
+            by_used_outpoint: HashMap::new(),
             pubkey_hash,
         };
     }
@@ -28,25 +31,39 @@ impl Wallet {
 
     pub fn remove(&mut self, outpoint: &Input) {
         self.by_outpoint.remove(outpoint);
+        self.by_used_outpoint.remove(outpoint);
     }
 
-    pub fn balance(&self) -> u64 {
+    pub fn total_balance(&self) -> u64 {
+        self.by_outpoint.values().map(|output|output.value).sum::<u64>()+
+            self.by_used_outpoint.values().map(|output|output.value).sum::<u64>()
+    }
+
+    pub fn safe_balance(&self) -> u64 {
         self.by_outpoint.values().map(|output|output.value).sum()
     }
 
     /// create a transaction using my coins
-    pub fn create(&self, recipient: H256, value: u64) -> Option<Transaction> {
+    pub fn create(&mut self, recipient: H256, value: u64) -> Option<Transaction> {
         let mut input: Vec<Input>= vec![];
-        let mut outpoint_iter = self.by_outpoint.iter();
         let mut value_sum = 0u64;
+
+        let mut outpoint_iter = self.by_outpoint.iter();
+        // first while, we use by_output rather than by_used_outpoint
         while let Some((outpoint,coin)) = outpoint_iter.next() {
             value_sum += coin.value;
             input.push(outpoint.clone());
             if value_sum >= value {
                 let mut output = vec![Output {recipient, value}];
                 if value_sum > value {
-                    /// the output that transfer remaining value to himself
+                    // the output that transfer remaining value to himself
                     output.push(Output{recipient: self.pubkey_hash.clone(), value: value_sum - value})
+                }
+                for outpoint in &input { // add inputs to used_outpoint to avoid potential double spend
+                    if let Some(coin) = self.by_outpoint.get(outpoint) {
+                        self.by_used_outpoint.insert(outpoint.clone(), coin.clone());
+                        self.by_outpoint.remove(outpoint);
+                    }
                 }
                 return Some(Transaction {
                     input,
@@ -55,6 +72,32 @@ impl Wallet {
                 });
             }
         }
+
+        outpoint_iter = self.by_used_outpoint.iter();
+        // second while, we use by_used_outpoint
+        while let Some((outpoint,coin)) = outpoint_iter.next() {
+            value_sum += coin.value;
+            input.push(outpoint.clone());
+            if value_sum >= value {
+                let mut output = vec![Output {recipient, value}];
+                if value_sum > value {
+                    // the output that transfer remaining value to himself
+                    output.push(Output{recipient: self.pubkey_hash.clone(), value: value_sum - value})
+                }
+                for outpoint in &input { // add inputs to used_outpoint to avoid potential double spend
+                    if let Some(coin) = self.by_outpoint.get(outpoint) {
+                        self.by_used_outpoint.insert(outpoint.clone(), coin.clone());
+                        self.by_outpoint.remove(outpoint);
+                    }
+                }
+                return Some(Transaction {
+                    input,
+                    output,
+                    signatures: vec![],
+                });
+            }
+        }
+
         return None;
     }
 
@@ -85,9 +128,10 @@ pub mod tests {
         let hash = crypto_generator::h256();
         let mut w = Wallet::new(hash.clone());
         w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: crypto_generator::h256()});
-        assert_eq!(w.balance(), 0);
+        assert_eq!(w.total_balance(), 0);
         w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
-        assert_eq!(w.balance(), 10);
+        assert_eq!(w.total_balance(), 10);
+        assert_eq!(w.safe_balance(), 10);
     }
 
     #[test]
@@ -98,7 +142,7 @@ pub mod tests {
         for i in 0..10 {
             w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
         }
-        assert_eq!(w.balance(), 100);
+        assert_eq!(w.total_balance(), 100);
         let tx = w.create(crypto_generator::h256(), 29);
         //println!("{:?}", tx);
         if let Some(tx) = tx {
@@ -113,39 +157,83 @@ pub mod tests {
     }
 
     #[test]
-    pub fn test_wallet_update_add_1() {
+    pub fn test_wallet_create_2() {
         let hash = crypto_generator::h256();
         let mut w = Wallet::new(hash.clone());
         // add 10*10 coins
         for i in 0..10 {
             w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
         }
-        assert_eq!(w.balance(), 100);
+        assert_eq!(w.total_balance(), 100);
+        // spend 5*20 coins
+        for i in 0..5 {
+            assert!(w.create(crypto_generator::h256(), 20).is_some());
+        }
+        // balance is still 100, but safe balance is 0
+        assert_eq!(w.total_balance(), 100);
+        assert_eq!(w.safe_balance(), 0);
+        // but all coins are marked as used
+        assert_eq!(w.by_used_outpoint.len(), 10);
+        // but we can still create tx using unsafe coins
+        assert!(w.create(crypto_generator::h256(), 1).is_some());
+    }
+
+    #[test]
+    pub fn test_wallet_create_3() {
+        let hash = crypto_generator::h256();
+        let mut w = Wallet::new(hash.clone());
+        // add 10*10 coins
+        for i in 0..10 {
+            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
+        }
+        assert_eq!(w.total_balance(), 100);
+        // spend 10*10 (although only 5 of 10) coins
+        for i in 0..10 {
+            assert!(w.create(crypto_generator::h256(), 5).is_some());
+        }
+        // balance is still 100, but safe balance is 0
+        assert_eq!(w.total_balance(), 100);
+        assert_eq!(w.safe_balance(), 0);
+        // but all coins are marked as used
+        assert_eq!(w.by_used_outpoint.len(), 10);
+        // but we can still create tx using unsafe coins
+        assert!(w.create(crypto_generator::h256(), 1).is_some());
+    }
+
+    #[test]
+    pub fn test_wallet_update_1() {
+        let hash = crypto_generator::h256();
+        let mut w = Wallet::new(hash.clone());
+        // add 10*10 coins
+        for i in 0..10 {
+            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
+        }
+        assert_eq!(w.total_balance(), 100);
         // spend 5*20 coins
         for i in 0..5 {
             assert!(w.create_update(crypto_generator::h256(), 20).is_some());
         }
         // now no coin can be spent
-        assert_eq!(w.balance(), 0);
+        assert_eq!(w.total_balance(), 0);
         assert!(w.create(crypto_generator::h256(), 1).is_none());
 
     }
 
     #[test]
-    pub fn test_wallet_update_add_2() {
+    pub fn test_wallet_update_2() {
         let hash = crypto_generator::h256();
         let mut w = Wallet::new(hash.clone());
         // add 10*10 coins
         for i in 0..10 {
             w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
         }
-        assert_eq!(w.balance(), 100);
+        assert_eq!(w.total_balance(), 100);
         // spend 20*5 coins
         for i in 0..20 {
             assert!(w.create_update(crypto_generator::h256(), 5).is_some());
         }
         // now no coin can be spent
-        assert_eq!(w.balance(), 0);
+        assert_eq!(w.total_balance(), 0);
         assert!(w.create(crypto_generator::h256(), 1).is_none());
 
     }
