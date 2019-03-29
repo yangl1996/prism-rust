7811e3be52568f795f7b1e91971a189e7ff1e8b8
Giulia Fanti <g.fanti@gmail.com>
2019-03-29 10:30:01 -0400

    fixed sortition in mining, simplified mining loop

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,18 @@
 // Constants
 
+// Block sizes
 pub const TX_BLOCK_SIZE: usize = 10;
 pub const PROPOSER_BLOCK_SIZE: usize = 10;
 pub const VOTER_BLOCK_SIZE: usize = 10;
 
 pub const TRANSACTION_INDEX: u32 = 0;
 pub const PROPOSER_INDEX: u32 = 1;
+
+// Mining rates in percentages of the total mining rate
+// Total for the voter chains
+pub const VOTER_MINING_RATE: u32 = 10;
+// Proposer tree
+pub const PROPOSER_MINING_RATE: u32 = 40;
+// Transaction blocks
+pub const TRANSACTION_MINING_RATE: u32 =
+    100 - PROPOSER_MINING_RATE - VOTER_MINING_RATE;
