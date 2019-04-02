47c497ae1fca6b75eb0b26e118b6ef7aa78dc492
Lei Yang <lei@yangl1996.com>
2019-04-01 22:02:29 -0400

    add method to get number of blocks in the db

--- src/blockdb/mod.rs
+++ src/blockdb/mod.rs
@@ -1,11 +1,13 @@
 use crate::block::Block;
 use crate::crypto::hash::H256;
 use bincode::{serialize, deserialize};
+use std::sync::{Mutex, Arc};
 
 pub type Result<T> = std::result::Result<T, rocksdb::Error>;
 
 pub struct BlockDatabase {
     handle: rocksdb::DB,
+    count: Mutex<u64>,
 }
 
 impl BlockDatabase {
@@ -13,12 +15,15 @@ impl BlockDatabase {
         let db_handle = rocksdb::DB::open_default(path)?;
         return Ok(BlockDatabase {
             handle: db_handle,
+            count: Mutex::new(0),
         });
     }
 
     pub fn insert(&self, hash: &H256, block: &Block) -> Result<()> {
         let hash_u8: [u8; 32] = hash.into();    // TODO: implement H256 asref<[u8]>
         let serialized = serialize(block).unwrap();
+        let mut count = self.count.lock().unwrap();
+        *count += 1;
         return self.handle.put(&hash_u8, &serialized);
     }
 
@@ -33,7 +38,14 @@ impl BlockDatabase {
 
     pub fn delete(&self, hash: &H256) -> Result<()> {
         let hash_u8: [u8; 32] = hash.into();
+        let mut count = self.count.lock().unwrap();
+        *count -= 1;
         return self.handle.delete(&hash_u8);
+    }
+
+    pub fn num_blocks(&self) -> u64 {
+        let count = self.count.lock().unwrap();
+        return *count;
     }
 }
 
@@ -49,8 +61,12 @@ mod tests {
         let test_block = generator::tx_block();
         db.insert(&test_block.hash(), &test_block);
         let got = db.get(&test_block.hash()).unwrap().unwrap();
+        let num_block = db.num_blocks();
         assert_eq!(got.hash(), test_block.hash());
+        assert_eq!(num_block, 1);
         db.delete(&test_block.hash());
+        let num_block = db.num_blocks();
         assert_eq!(db.get(&test_block.hash()).unwrap().is_none(), true);
+        assert_eq!(num_block, 0);
     }
 }
