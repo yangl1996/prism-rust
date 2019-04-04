837e59ffe58b2f45a78d369670be937ec6315061
Lei Yang <lei@yangl1996.com>
2019-04-04 16:21:53 -0400

    start miner in main

--- src/network/mod.rs
+++ src/network/mod.rs
@@ -3,21 +3,3 @@ pub mod server;
 pub mod peer;
 pub mod worker;
 
-use crate::blockdb::BlockDatabase;
-use crate::blockchain::BlockChain;
-use crate::miner::memory_pool::MemoryPool;
-use crate::config;
-use std::sync::{Mutex, mpsc, Arc};
-
-pub fn start(addr: std::net::SocketAddr, blockdb: &Arc<BlockDatabase>, 
-             blockchain: &Arc<Mutex<BlockChain>>,
-             mempool: &Arc<Mutex<MemoryPool>>) -> std::io::Result<server::Handle> {
-    let (msg_sink, msg_source) = mpsc::channel();
-    let (ctx, server) = server::new(addr, msg_sink)?;
-    ctx.start();
-
-    let ctx = worker::new(4, msg_source, blockchain, blockdb, mempool);
-    ctx.start();
-
-    return Ok(server);
-}
