3037d597f8e4e487e5b0af249aa5bb45c3f973af
Vivek Bagaria <vivekee047@gmail.com>
2019-04-03 18:46:54 -0700

    Lifetime issues

--- src/miner/mod.rs
+++ src/miner/mod.rs
@@ -1,2 +1,15 @@
 pub mod memory_pool;
 pub mod miner;
+
+
+// Instruction set for the miner
+pub enum Instruction{
+    StartMining,
+    StopMining,
+    NewContent
+// To be added later for efficiency
+//    NewTx,
+//    NewTxBlockContent,
+//    NewPropBlockContent,
+//    NewVoterBlockContent(u16)
+}
