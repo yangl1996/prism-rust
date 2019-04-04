f3367eb47af06dadde9ec77ee70bef2973bd7d5b
Lei Yang <lei@yangl1996.com>
2019-04-04 19:52:19 -0400

    add channel between worker and miner

--- src/lib.rs
+++ src/lib.rs
@@ -26,6 +26,7 @@ use std::sync::{Mutex, mpsc, Arc};
 pub fn start(addr: std::net::SocketAddr, blockdb: &Arc<BlockDatabase>, 
              blockchain: &Arc<Mutex<BlockChain>>,
              mempool: &Arc<Mutex<MemoryPool>>) -> std::io::Result<(network::server::Handle, miner::miner::Handle)> {
+    // create channels between server and worker, worker and miner, miner and worker
     let (msg_sink, msg_source) = mpsc::channel();
     let (ctx_update_sink, ctx_update_source) = mpsc::channel();
     let (mined_block_sink, mined_block_source) = mpsc::channel();
@@ -33,7 +34,7 @@ pub fn start(addr: std::net::SocketAddr, blockdb: &Arc<BlockDatabase>,
     let (ctx, server) = network::server::new(addr, msg_sink)?;
     ctx.start();
 
-    let ctx = network::worker::new(4, msg_source, blockchain, blockdb, mempool);
+    let ctx = network::worker::new(4, msg_source, blockchain, blockdb, mempool, ctx_update_sink);
     ctx.start();
 
     let (ctx, miner) = miner::miner::new(mempool, blockchain, blockdb, mined_block_sink, ctx_update_source);
