b920e69453392c9e4a5b6d3597716d3723baf8f9
Lei Yang <lei@yangl1996.com>
2019-04-08 18:35:08 -0400

    add visualization

--- src/visualization/server.rs
+++ src/visualization/server.rs
@@ -19,7 +19,9 @@ impl Server {
         };
         thread::spawn(move || {
             for req in server.handle.incoming_requests() {
-                let resp = Response::from_string(dump_blockchain(&server.chain.lock().unwrap()));
+                let allow_all_origin = tiny_http::Header::from_bytes(&b"Access-Control-Allow-Origin"[..], &b"*"[..]).unwrap();
+                let resp = Response::from_string(dump_blockchain(&server.chain.lock().unwrap()))
+                    .with_header(allow_all_origin);
                 req.respond(resp);
             }
         });
