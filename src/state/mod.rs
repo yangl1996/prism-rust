5ca05d8439cbff770bc8bb2d0a5eb5dfa74f7ce6
Vivek Bagaria <vivekee047@gmail.com>
2019-04-08 20:07:36 -0700

    UTXO database modified

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,11 +1,23 @@
-use crate::transaction::{Input, Output};
+use crate::transaction::{Input, Output, Transaction};
 use bincode::{deserialize, serialize};
+use std::hash::{Hash, Hasher};
+use std::collections::hash_map::DefaultHasher;
+use std::sync::Mutex;
 
 pub type Result<T> = std::result::Result<T, rocksdb::Error>;
+
+#[derive(Hash, Serialize)]
+pub struct UTXO{
+    transaction: Transaction, //TODO: We only need the hash of the tx and index.
+    index: u32,
+    value: u64
+}
+
 
 #[derive(Debug)]
 pub struct UTXODatabase {
     handle: rocksdb::DB,
+    count: Mutex<u64>,
 }
 
 impl UTXODatabase {
@@ -13,18 +25,42 @@ impl UTXODatabase {
         let db_handle = rocksdb::DB::open_default(path)?;
         return Ok(UTXODatabase {
             handle: db_handle,
+            count: Mutex::new(0),
         });
     }
 
-    pub fn insert(&self, input: &Input, output: &Output) -> Result<()> {
-        let input_serialized = serialize(input).unwrap();
-        let output_serialized = serialize(output).unwrap();
-        return self.handle.put(&input_serialized, &output_serialized);
+    pub fn insert(&self, utxo: &UTXO) -> Result<()> {
+        let mut hasher = DefaultHasher::new();
+        let utxo_hash = serialize(& utxo.hash(&mut hasher)).unwrap();
+        let utxo_serialized = serialize(utxo).unwrap();
+        let mut count = self.count.lock().unwrap();
+        *count += 1;
+        return self.handle.put(&utxo_hash, &utxo_serialized);
     }
 
-    pub fn delete(&mut self, input: &Input) -> Result<()> {
-        let input_serialized = serialize(input).unwrap();
-        return self.handle.delete(&input_serialized);
+    pub fn delete(&mut self, utxo: &UTXO) -> Result<()> { //TODO: Should accept a hash of the utxo.
+        let mut hasher = DefaultHasher::new();
+        let utxo_hash = serialize(&utxo.hash(&mut hasher)).unwrap();
+        let mut count = self.count.lock().unwrap();
+        *count -= 1;
+        return self.handle.delete(utxo_hash);
+    }
+
+    //TODO: Should accept a hash of the utxo.
+    //TODO: Only check if the utxo exists. It should not 'get' it.
+    pub fn check(&self, utxo: &UTXO) -> Result<bool> {
+        let mut hasher = DefaultHasher::new();
+        let utxo_hash = serialize(&utxo.hash(&mut hasher)).unwrap();
+        let serialized = self.handle.get(&utxo_hash)?;
+        match serialized {
+            None => return Ok(false),
+            Some(s) => return Ok(true),
+        }
+    }
+
+    pub fn num_utxo(&self) -> u64 {
+        let count = self.count.lock().unwrap();
+        return *count;
     }
 }
 
