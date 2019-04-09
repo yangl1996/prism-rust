a17a95ababc85b840bec15c15a4197323c1b36d3
wgr523 <wgr523@gmail.com>
2019-04-09 15:33:08 -0500

    modify test for wallet

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,4 +1,3 @@
-pub mod generator;
 
 use crate::block::transaction::Content as TxContent;
 use crate::crypto::hash::{Hashable, H256};
@@ -13,6 +12,7 @@ pub type Result<T> = std::result::Result<T, rocksdb::Error>;
 pub type CoinId = Input;
 
 // Bitcoin UTXO is much more complicated because they have extra seg-wit and locktime.
+#[derive(Serialize, Deserialize, Clone, Hash, Eq, PartialEq)]
 pub struct UTXO {
     pub coin_id: CoinId, // Hash of the transaction. This along with the index is the coin index is the key.
     pub value: u64,
@@ -72,41 +72,3 @@ impl UTXODatabase {
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use crate::transaction::generator as tx_generator;
-
-    #[test]
-    fn insert_get_check_and_delete() {
-        let mut state_db = generator::random();
-        let mut count = state_db.num_utxo();
-
-        println!("Test 1: count");
-        let transaction = tx_generator::random();
-        let utxos = generator::tx_to_utxos(transaction);
-        for utxo in utxos.iter() {
-            state_db.insert(utxo);
-        }
-
-        assert_eq!(state_db.num_utxo(), count + utxos.len() as u64);
-
-        println!("Test 2: check()");
-        for utxo in utxos.iter() {
-            assert!(state_db.check(&utxo.coin_id).unwrap());
-        }
-
-        println!("Test 3: get()");
-        for utxo in utxos.iter() {
-            assert_eq!(state_db.get(&utxo.coin_id).unwrap().unwrap(), utxo.value);
-        }
-
-        println!("Test 4: delete()");
-        state_db.delete(&utxos[0].coin_id);
-        assert!(!state_db.check(&utxos[0].coin_id).unwrap());
-
-        assert_eq!(state_db.num_utxo(), count + utxos.len() as u64 - 1);
-    }
-}
-
-// TODO: add tests
--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -3,15 +3,15 @@ use crate::crypto::sign::{KeyPair, Signature};
 use crate::miner::memory_pool::MemoryPool;
 use crate::miner::miner::ContextUpdateSignal;
 use crate::transaction::{Input, Output, Transaction};
-use log::error;
 use std::collections::{HashMap, HashSet};
 use std::sync::mpsc;
 use std::sync::{Arc, Mutex};
+use crate::state::{UTXO, CoinId};
 
 #[derive(Clone, Hash, PartialEq, Eq)]
 pub struct Coin {
-    input: Input,
-    output: Output,
+    utxo: UTXO,
+    recipient: H256,
 }
 
 pub struct Wallet {
@@ -25,6 +25,9 @@ pub struct Wallet {
     mempool: Arc<Mutex<MemoryPool>>,
 }
 
+pub enum WalletError {
+    InsufficientCoin,
+}
 impl Wallet {
     pub fn new(
         mempool: &Arc<Mutex<MemoryPool>>,
@@ -49,17 +52,16 @@ impl Wallet {
 
     /// Add coins in a transaction that are destined to us
     pub fn add_transaction(&mut self, tx: &Transaction) {
-        let hash = tx.hash();
+        let hash = tx.hash();// compute hash here, and below inside Input we don't have to compute again (we just copy)
         for (idx, output) in tx.output.iter().enumerate() {
             if self.keys.contains_key(&output.recipient) {
-                let input = Input {
+                let input = CoinId {
                     hash,
                     index: idx as u32,
                 };
-                let output = output.clone();
                 let coin = Coin {
-                    input: input,
-                    output: output,
+                    utxo: UTXO {coin_id: input, value: output.value},
+                    recipient: output.recipient,
                 };
                 self.coins.insert(coin);
             }
@@ -73,7 +75,7 @@ impl Wallet {
 
     ///  Returns the sum of values of all the coin in the wallet
     pub fn balance(&self) -> u64 {
-        self.coins.iter().map(|coin| coin.output.value).sum::<u64>()
+        self.coins.iter().map(|coin| coin.utxo.value).sum::<u64>()
     }
 
     /// create a transaction using the wallet coins
@@ -81,27 +83,20 @@ impl Wallet {
         let mut coins: Vec<Coin> = vec![];
         let mut value_sum = 0u64;
 
+        let iter_coins = self.coins.iter();
         // iterate thru our wallet
-        for coin in self.coins.iter().cloned() {
-            // TODO: can we remove cloned here?
-            value_sum += coin.output.value;
+        for coin in iter_coins {
+            value_sum += coin.utxo.value;
             coins.push(coin.clone()); // coins that will be used for this transaction
 
             if value_sum >= value {
                 // if we have enough money in our wallet, create tx
-                // first, create transaction inputs
-                let mut input: Vec<Input> = vec![];
-                let _signatures: Vec<Signature> = vec![];
-
-                for c in coins {
-                    input.push(c.input.clone());
-                    self.remove_coin(&c);
-                }
-
+                // create transaction inputs
+                let input = coins.iter().map(|c|c.utxo.coin_id.clone()).collect();
                 // create the output
                 let mut output = vec![Output { recipient, value }];
                 if value_sum > value {
-                    // transfer the remaining value back to ourself
+                    // transfer the remaining value back to self
                     let recipient: H256 = match self.keys.keys().next() {
                         Some(&x) => x,
                         None => panic!("The wallet has no keys"),
@@ -112,7 +107,12 @@ impl Wallet {
                     })
                 }
 
-                // TODO: sign the transaction
+                // remove used coin from wallet
+                for c in &coins {
+                    self.remove_coin(c);
+                }
+
+                // TODO: sign the transaction use coins
                 return Some(Transaction {
                     input,
                     output,
@@ -123,23 +123,20 @@ impl Wallet {
         return None;
     }
 
-    pub fn send_coin(&mut self, recipient: H256, value: u64) {
+    pub fn send_coin(&mut self, recipient: H256, value: u64) -> Result<(),WalletError> {
         let txn = self.create_transaction(recipient, value);
         let txn = match txn {
             Some(t) => t,
             None => {
-                // TODO: error handling
-                error!("Insufficient wallet balance.");
-                return;
+                return Err(WalletError::InsufficientCoin);
             }
         };
         let mut mempool = self.mempool.lock().unwrap();
         mempool.insert(txn);
         drop(mempool);
         self.context_update_chan
-            .send(ContextUpdateSignal::NewContent)
-            .unwrap();
-        return;
+            .send(ContextUpdateSignal::NewContent).unwrap();
+        return Ok(());
     }
 }
 
@@ -150,6 +147,7 @@ pub mod tests {
     use crate::crypto::generator as crypto_generator;
     use crate::miner::memory_pool::MemoryPool;
     use std::sync::{mpsc, Arc, Mutex};
+    use crate::miner::miner::ContextUpdateSignal;
 
     #[test]
     pub fn test_balance() {
@@ -209,6 +207,9 @@ pub mod tests {
         for tx in &txs {
             println!("{:?}",tx);
         }
+        for i in 0..5 {
+            ctx_update_source.recv().unwrap();
+        }
     }
 
     #[test]
@@ -240,110 +241,8 @@ pub mod tests {
         for tx in &txs {
             println!("{:?}",tx);
         }
-    }
-/*
-    #[test]
-    pub fn test_wallet_create() {
-        let hash = crypto_generator::h256();
-        let mut w = Wallet::new(hash.clone());
-        // add 10*10 coins
-        for i in 0..10 {
-            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
-        }
-        assert_eq!(w.total_balance(), 100);
-        let tx = w.create(crypto_generator::h256(), 29);
-        //println!("{:?}", tx);
-        if let Some(tx) = tx {
-            // This transaction should be input(10,10,10) output(29,1)
-            assert_eq!(tx.input.len(),3);
-            assert_eq!(tx.output.len(),2);
-        } else {
-            panic!("transaction creation failed")
-        }
-
-        assert!(w.create(crypto_generator::h256(), 10000).is_none());
-    }
-
-    #[test]
-    pub fn test_wallet_create_2() {
-        let hash = crypto_generator::h256();
-        let mut w = Wallet::new(hash.clone());
-        // add 10*10 coins
-        for i in 0..10 {
-            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
-        }
-        assert_eq!(w.total_balance(), 100);
-        // spend 5*20 coins
         for i in 0..5 {
-            assert!(w.create(crypto_generator::h256(), 20).is_some());
+            ctx_update_source.recv().unwrap();
         }
-        // balance is still 100, but safe balance is 0
-        assert_eq!(w.total_balance(), 100);
-        assert_eq!(w.safe_balance(), 0);
-        // but all coins are marked as used
-        assert_eq!(w.by_used_outpoint.len(), 10);
-        // but we can still create tx using unsafe coins
-        assert!(w.create(crypto_generator::h256(), 1).is_some());
     }
-
-    #[test]
-    pub fn test_wallet_create_3() {
-        let hash = crypto_generator::h256();
-        let mut w = Wallet::new(hash.clone());
-        // add 10*10 coins
-        for i in 0..10 {
-            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
-        }
-        assert_eq!(w.total_balance(), 100);
-        // spend 10*10 (although only 5 of 10) coins
-        for i in 0..10 {
-            assert!(w.create(crypto_generator::h256(), 5).is_some());
-        }
-        // balance is still 100, but safe balance is 0
-        assert_eq!(w.total_balance(), 100);
-        assert_eq!(w.safe_balance(), 0);
-        // but all coins are marked as used
-        assert_eq!(w.by_used_outpoint.len(), 10);
-        // but we can still create tx using unsafe coins
-        assert!(w.create(crypto_generator::h256(), 1).is_some());
-    }
-
-    #[test]
-    pub fn test_wallet_update_1() {
-        let hash = crypto_generator::h256();
-        let mut w = Wallet::new(hash.clone());
-        // add 10*10 coins
-        for i in 0..10 {
-            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
-        }
-        assert_eq!(w.total_balance(), 100);
-        // spend 5*20 coins
-        for i in 0..5 {
-            assert!(w.create_update(crypto_generator::h256(), 20).is_some());
-        }
-        // now no coin can be spent
-        assert_eq!(w.total_balance(), 0);
-        assert!(w.create(crypto_generator::h256(), 1).is_none());
-
-    }
-
-    #[test]
-    pub fn test_wallet_update_2() {
-        let hash = crypto_generator::h256();
-        let mut w = Wallet::new(hash.clone());
-        // add 10*10 coins
-        for i in 0..10 {
-            w.insert(Input{hash: crypto_generator::h256(), index: 0}, Output{value: 10, recipient: hash.clone()});
-        }
-        assert_eq!(w.total_balance(), 100);
-        // spend 20*5 coins
-        for i in 0..20 {
-            assert!(w.create_update(crypto_generator::h256(), 5).is_some());
-        }
-        // now no coin can be spent
-        assert_eq!(w.total_balance(), 0);
-        assert!(w.create(crypto_generator::h256(), 1).is_none());
-
-    }
-*/
 }
