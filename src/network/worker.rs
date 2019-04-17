337a999133daedffb28758cba1daa8b6cd3cdaab
Lei Yang <lei@yangl1996.com>
2019-04-17 18:54:01 -0400

    exchange blocks with peers

--- src/network/worker.rs
+++ src/network/worker.rs
@@ -7,6 +7,8 @@ use crate::miner::miner::ContextUpdateSignal;
 use log::info;
 use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
+use crate::handler::new_validated_block;
+use crate::network::server::Handle as ServerHandle;
 
 #[derive(Clone)]
 pub struct Context {
@@ -16,6 +18,7 @@ pub struct Context {
     blockdb: Arc<BlockDatabase>,
     mempool: Arc<Mutex<MemoryPool>>,
     context_update_chan: mpsc::Sender<ContextUpdateSignal>,
+    server: ServerHandle,
 }
 
 pub fn new(
@@ -25,6 +28,7 @@ pub fn new(
     blockdb: &Arc<BlockDatabase>,
     mempool: &Arc<Mutex<MemoryPool>>,
     ctx_update_sink: mpsc::Sender<ContextUpdateSignal>,
+    server: ServerHandle
 ) -> Context {
     let ctx = Context {
         msg_chan: Arc::new(Mutex::new(msg_src)),
@@ -33,6 +37,7 @@ pub fn new(
         blockdb: Arc::clone(blockdb),
         mempool: Arc::clone(mempool),
         context_update_chan: ctx_update_sink,
+        server: server
     };
     return ctx;
 }
@@ -61,6 +66,40 @@ impl Context {
                 }
                 Message::Pong(nonce) => {
                     info!("Pong: {}", nonce);
+                }
+                Message::NewBlockHashes(hashes) => {
+                    let mut hashes_to_request = vec![];
+                    for hash in hashes {
+                        // TODO: add a method to blockdb to quickly check whether a block exists
+                        match self.blockdb.get(&hash).unwrap() {
+                            None => {
+                                hashes_to_request.push(hash);
+                            }
+                            _ => {
+                            }
+                        }
+                    }
+                    peer.write(Message::GetBlocks(hashes_to_request));
+                }
+                Message::GetBlocks(hashes) => {
+                    let mut blocks = vec![];
+                    for hash in hashes {
+                        match self.blockdb.get(&hash).unwrap() {
+                            None => {
+                            }
+                            Some(block) => {
+                                blocks.push(block);
+                            }
+                        }
+                    }
+                    peer.write(Message::Blocks(blocks));
+                }
+                Message::Blocks(blocks) => {
+                    // TODO: add validation and buffer logic here
+                    for block in blocks {
+                        // TODO: avoid inserting the same block again here
+                        new_validated_block(block, &self.blockdb, &self.chain, &self.server);
+                    }
                 }
                 _ => {
                 }
