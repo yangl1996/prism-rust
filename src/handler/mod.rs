a260426373abe183442b836a3d275ba45f27ecc0
wgr523 <wgr523@gmail.com>
2019-04-17 22:19:22 -0500

    fix small bug of remove_unreferred transaction block hash

--- src/handler/mod.rs
+++ src/handler/mod.rs
@@ -6,4 +6,6 @@ pub use new_transaction::new_transaction;
 pub use new_validated_block::new_validated_block;
 pub use state_update::to_utxo;
 pub use state_update::to_rollback_utxo;
+pub use state_update::conf;
+pub use state_update::to_rollback_utxo;
 
