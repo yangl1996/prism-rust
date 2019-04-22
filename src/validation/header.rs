f3254bcd2ff082818de757b7fe8af3ea6f45278f
Lei Yang <lei@yangl1996.com>
2019-04-22 14:24:11 -0400

    refactor validation rules for header

--- src/validation/header.rs
+++ src/validation/header.rs
@@ -8,7 +8,7 @@ use crate::crypto::hash::{Hashable, H256};
 use bigint::uint::U256;
 use std::sync::{Arc, Mutex};
 
-fn check_pow(block: &Block, proposer_range: (U256, U256), voter_ranges: Vec<(U256, U256)>, transaction_range: (U256, U256)) -> BlockResult {
+pub fn check_pow(block: &Block, proposer_range: (U256, U256), voter_ranges: Vec<(U256, U256)>, transaction_range: (U256, U256)) -> BlockResult {
     // TODO: avoid doing those conversions
     let hash: [u8; 32] = (&block.header.hash()).into();
     let hash = U256::from_big_endian(&hash);
@@ -26,7 +26,7 @@ fn check_pow(block: &Block, proposer_range: (U256, U256), voter_ranges: Vec<(U25
             return BlockResult::Fail;
         }
         Content::Voter(c) => {
-            let voter_range = self.voter_ranges[c.chain_number as usize];
+            let voter_range = voter_ranges[c.chain_number as usize];
             if voter_range.0 <= hash && hash <= voter_range.1 {
                 return BlockResult::Pass;
             }
@@ -35,8 +35,8 @@ fn check_pow(block: &Block, proposer_range: (U256, U256), voter_ranges: Vec<(U25
     }
 }
 
-fn check_sortition(block: &Block, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase) -> BlockResult {
-    //1. Check parent proposer block is available
+pub fn check_sortition(block: &Block, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase) -> BlockResult {
+    // 1. Check parent proposer block is available
     let proposer_parent_hash = block.header.parent_hash;
     let proposer_parent = data_availability::get_block(
         proposer_parent_hash,
@@ -47,34 +47,21 @@ fn check_sortition(block: &Block, blockchain: &Mutex<BlockChain>, blockdb: &Bloc
         Ok(block) => {
             // good, do nothing
         }
-        BlockDataAvailability::NotInDB => {
-            // The voter parent should be requested from the network
-            return BlockRuleResult::MissingReferencesInDBandBC(
-                vec![proposer_parent_hash],
-                vec![],
-            );
-        }
-        BlockDataAvailability::NotInBlockchain => {
-            // The voter parent should be added to the blockchain first
-            return BlockRuleResult::MissingReferencesInDBandBC(
-                vec![],
-                vec![proposer_parent_hash],
-            );
-        }
-        BlockDataAvailability::Block(proposer_parent_block) => {
-            // do nothing
+        Err(_) => {
+            return BlockResult::MissingReferences(vec![proposer_parent_hash]);
         }
     }
-    //2. Check sortition proof
-    if check_sortition(block) {
-        return BlockRuleResult::True;
+
+    // 2. Check sortition proof
+    if check_sortition_proof(block) {
+        return BlockResult::Pass;
     }
 
     unimplemented!();
 }
 
 /// Checks the sortition logic
-pub fn check_sortition(block: &Block) -> bool {
+pub fn check_sortition_proof(block: &Block) -> bool {
     //1. First check if hash(content) =? content_root
     match &block.content {
         Content::Transaction(content) => {
