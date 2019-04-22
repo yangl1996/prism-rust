254bed1ede9d0e677be052f985dd8367434887ad
Lei Yang <lei@yangl1996.com>
2019-04-22 17:26:36 -0400

    refactor validation

--- src/validation/mod.rs
+++ src/validation/mod.rs
@@ -1,18 +1,121 @@
-pub mod data_availability;
-pub mod header;
-pub mod transaction;
-pub mod transaction_block;
-pub mod proposer_block;
-pub mod voter_block;
-use crate::block::Block;
+mod transaction;
+mod proposer_block;
+mod voter_block;
+use crate::block::{Block, Content};
 use crate::blockchain::BlockChain;
 use crate::blockdb::BlockDatabase;
+use crate::state::UTXODatabase;
 use crate::crypto::hash::{Hashable, H256};
 use crate::transaction::Transaction;
-use std::sync::{Arc, Mutex};
+use std::sync::Mutex;
 
+/// The result of block validation.
 pub enum BlockResult {
+    /// The validation passes.
     Pass,
+    /// The parent block is missing.
+    MissingParent(H256),
+    /// The content hash does not match.
+    WrongContentHash,
+    /// Some references are missing.
     MissingReferences(Vec<H256>),
-    Fail,
+    /// A voter block has a out-of-range chain number.
+    WrongChainNumber,
+    /// A voter block votes for incorrect proposer levels.
+    WrongVoteLevel,
+    EmptyTransaction,
+    InputAlreadySpent,
+    InsufficientInput,
+    WrongSignature,
+}
+
+/// Validate a block.
+pub fn check_block(block: &Block, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase, utxodb: &UTXODatabase) -> BlockResult {
+    // TODO: check PoW. Where should we get the current difficulty ranges?
+
+    // check whether the parent exists
+    let parent = block.header.parent_hash;
+    let parent_availability = check_block_exist(parent, blockchain, blockdb);
+    if !(parent_availability.0 && parent_availability.1) {
+        return BlockResult::MissingParent(parent);
+    }
+
+    // TODO: check timestamp
+    // TODO: check sortition proof
+
+    // check the content hash
+    if block.content.hash() != block.header.content_root {
+        return BlockResult::WrongContentHash;
+    }
+
+    // match the block type and check content
+    match &block.content {
+        Content::Transaction(content) => {
+            // check each transaction
+            for transaction in content.transactions.iter() {
+                if !transaction::check_non_empty(&transaction) {
+                    return BlockResult::EmptyTransaction;
+                }
+
+                if !transaction::check_input_unspent(&transaction, utxodb) {
+                    return BlockResult::InputAlreadySpent;
+                }
+
+                if !transaction::check_sufficient_input(&transaction) {
+                    return BlockResult::InsufficientInput;
+                }
+
+                if !transaction::check_signature(&transaction) {
+                    return BlockResult::WrongSignature;
+                }
+            }
+            return BlockResult::Pass;
+        }
+        Content::Proposer(content) => {
+            // check for missing references
+            let missing_refs = proposer_block::get_missing_references(&content, blockchain, blockdb);
+            if missing_refs.len() == 0 {
+                return BlockResult::Pass;
+            }
+            else {
+                return BlockResult::MissingReferences(missing_refs);
+            }
+        }
+        Content::Voter(content) => {
+            // check for missing references
+            let missing_refs = voter_block::get_missing_references(&content, blockchain, blockdb);
+            if missing_refs.len() != 0 {
+                return BlockResult::MissingReferences(missing_refs);
+            }
+
+            // check chain number
+            if !voter_block::check_chain_number(&content) {
+                return BlockResult::WrongChainNumber;
+            }
+
+            // check whether all proposer levels deeper than the one our parent voted are voted
+            if !voter_block::check_levels_voted(&content, blockchain, blockdb) {
+                return BlockResult::WrongVoteLevel;
+            }
+
+            return BlockResult::Pass;
+        }
+    }
+}
+
+/// Check whether a block exists in the blockchain and the block database. The function returns a
+/// tuple, of which the first member being whether the block is in the block database, and the
+/// second one the block chain.
+fn check_block_exist(hash: H256, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase) -> (bool, bool) {
+    let in_db = match blockdb.get(&hash) {
+        Err(e) => panic!("Database error {}", e),
+        Ok(b) => match b {
+            None => false,
+            Some(_) => true,
+        }
+    };
+
+    let in_blockchain = blockchain.lock().unwrap().check_node(hash);
+
+    return (in_db, in_blockchain);
 }
--- src/validation/voter_block.rs
+++ src/validation/voter_block.rs
@@ -1,107 +1,84 @@
 use super::super::config;
-use super::data_availability::{self, get_block};
-use super::*;
 use crate::block::Block;
-use crate::block::Content;
+use crate::block::voter::Content;
+use crate::block::Content as BlockContent;
 use crate::blockchain::BlockChain;
-use crate::blockchain::utils::get_voter_genesis_hash;
 use crate::blockdb::BlockDatabase;
+use crate::blockchain::utils::get_voter_genesis_hash;
 use crate::crypto::hash::{Hashable, H256};
-use std::sync::{Arc, Mutex};
-use super::BlockResult;
+use std::sync::Mutex;
+use super::check_block_exist;
 
-fn validate(block: &Block, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase) -> BlockResult {
-    let content = match &block.content {
-        Content::Voter(content) => content,
-        _ => panic!("Wrong type"),
-    };
+pub fn get_missing_references(content: &Content, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase) -> Vec<H256> {
+    let mut missing_blocks = vec![];
 
-    // Check 1: If the chain number is valid
-    let chain_number = content.chain_number;
-    if chain_number < 0 || chain_number > config::NUM_VOTER_CHAINS {
-        return BlockResult::Fail;
+    // check the voter parent
+    let voter_parent = check_block_exist(content.voter_parent_hash, blockchain, blockdb);
+    if !(voter_parent.0 && voter_parent.1) {
+        missing_blocks.push(content.voter_parent_hash);
     }
-    let mut missing_blocks: Vec<H256> = vec![];
 
-    // Check 2. If the parent voter block is available
-    let mut latest_level_voted_by_ancestor: usize = 0;
-    let voter_parent =
-        get_block(content.voter_parent_hash, blockchain, blockdb);
-    match voter_parent {
-        Ok(voter_parent_block) => {
-            latest_level_voted_by_ancestor = latest_level_voted_on_chain(&voter_parent_block,
-                                                                         blockchain,
-                                                                         blockdb);
-        }
-        Err(data_availability::Error::MissingInDB) => {
-            missing_blocks.push(content.voter_parent_hash);
-        }
-        Err(data_availability::Error::MissingInBlockchain) => {
-            missing_blocks.push(content.voter_parent_hash);
+    // check the votes
+    for prop_hash in content.proposer_block_votes.iter() {
+        let avail = check_block_exist(*prop_hash, blockchain, blockdb);
+        if !(avail.0 && avail.1) {
+            missing_blocks.push(*prop_hash);
         }
     }
 
-    // Check 3. If all voted proposer blocks are available and are on continuous level from
-    // latest_level_voted_by_ancestor onwards
-    for (index, proposer_vote) in content.proposer_block_votes.iter().enumerate() {
-        let proposer_block =
-            get_block(*proposer_vote, blockchain, blockdb);
-        match proposer_block {
-            Ok(block) => {
-                let blockchain_l = blockchain.lock().unwrap();
-                let level = blockchain_l.prop_node_data(&block.hash()).level as usize;
-                drop(blockchain_l);
-                if level != index + 1 + latest_level_voted_by_ancestor {
-                    //The votes are not on continuous levels and hence the block is invalid.
-                    return BlockResult::Fail;
-                }
-            }
-            Err(data_availability::Error::MissingInDB) => {
-                missing_blocks.push(*proposer_vote);
-            }
-            Err(data_availability::Error::MissingInBlockchain) => {
-                missing_blocks.push(*proposer_vote);
-            }
-        }
-    }
+    return missing_blocks;
+}
 
-    //Final result: If all the data is available then the block is valid
-    if missing_blocks.len() == 0 {
-        return BlockResult::Pass;
+pub fn check_chain_number(content: &Content) -> bool {
+    let chain_num = content.chain_number;
+    if chain_num > config::NUM_VOTER_CHAINS {
+        return false;
     } else {
-        return BlockResult::MissingReferences(missing_blocks);
+        return true;
     }
 }
 
-/// Returns the last proposer level voted by the voter chain until 'voter_block'
+pub fn check_levels_voted(content: &Content, blockchain: &Mutex<BlockChain>, blockdb: &BlockDatabase) -> bool {
+    // get the deepest block voted by our parent
+    let parent_block = blockdb.get(&content.voter_parent_hash).unwrap().unwrap();
+    let mut last_voted_level = latest_level_voted_on_chain(&parent_block, blockchain, blockdb);
+
+    // check whether the votes are continuous, and starts at the next unvoted level
+    for (index, proposer_vote) in content.proposer_block_votes.iter().enumerate() {
+        let voted = blockdb.get(proposer_vote).unwrap().unwrap();
+        let level = blockchain.lock().unwrap().prop_node_data(&voted.hash()).level as usize;
+        if level != index + 1 + last_voted_level {
+            return false
+        }
+    }
+
+    return true;
+}
+
+/// Get the deepest proposer level voted by this chain, until the given voter block.
 fn latest_level_voted_on_chain(
     voter_block: &Block,
     blockchain: &Mutex<BlockChain>,
     blockdb: &BlockDatabase,
 ) -> usize {
     let content = match &voter_block.content {
-        Content::Voter(content) => content,
+        BlockContent::Voter(content) => content,
         _ => panic!("Wrong type"),
     };
 
     let voter_genesis_hash = get_voter_genesis_hash(content.chain_number);
-    // Base case
+    
     if voter_block.hash() == voter_genesis_hash {
+        // if the voter block is the genesis block
         return 0;
     } else if content.proposer_block_votes.len() > 0 {
-        // If the block content has any votes, then return the latest voted level
+        // if this block voted for some blocks, then just return the deepest level among them
         let latest_prop_voted = content.proposer_block_votes.last().unwrap();
-        let blockchain_l = blockchain.lock().unwrap();
-        return blockchain_l.prop_node_data(latest_prop_voted).level as usize;
+        return blockchain.lock().unwrap().prop_node_data(latest_prop_voted).level as usize;
     } else {
-        // Else call the function on its voter parent block
-        let voter_parent = get_block(content.voter_parent_hash, blockchain, blockdb);
-        match voter_parent {
-            Ok(voter_block_inner) => {
-                return latest_level_voted_on_chain(&voter_block_inner, blockchain, blockdb);
-            }
-            _ => panic!("This shouldn't have happened! The parent block should be there in both db and bc."),
-        }
+        // if this block voted for zero block, then look for its parent
+        let parent = blockdb.get(&content.voter_parent_hash).unwrap().unwrap();
+        return latest_level_voted_on_chain(&parent, blockchain, blockdb);
     }
 }
 
