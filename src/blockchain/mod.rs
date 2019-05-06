bf3b43fc410b9ca6ec058f3d8ded9ba0104db7b7
Lei Yang <lei@yangl1996.com>
2019-05-06 15:47:21 -0400

    track chain tips of proposer and voter

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -7,8 +7,6 @@ use bincode::{deserialize, serialize};
 use rocksdb::{ColumnFamily, Options, ColumnFamilyDescriptor, DB, WriteBatch};
 use std::collections::{HashMap, HashSet, BTreeMap};
 
-use std::iter::FromIterator;
-
 // Column family names for node/chain metadata
 const PROPOSER_NODE_LEVEL_CF: &str = "PROPOSER_NODE_LEVEL";
 const PROPOSER_NODE_STATUS_CF: &str = "PROPOSER_NODE_STATUS";
@@ -31,6 +29,8 @@ pub type Result<T> = std::result::Result<T, rocksdb::Error>;
 
 pub struct BlockChain {
     db: DB,
+    proposer_best: Mutex<(H256, u64)>,
+    voter_best: Vec<Mutex<(H256, u64)>>,
 }
 
 // Functions to edit the blockchain
@@ -88,9 +88,15 @@ impl BlockChain {
         opts.create_if_missing(true);
         opts.create_missing_column_families(true);
         let db = DB::open_cf_descriptors(&opts, path, cfs)?;
+        let mut voter_best: Vec<Mutex<(H256, u64)>> = vec![];
+        for _ in 0..NUM_VOTER_CHAINS {
+            voter_best.push(Mutex::new((H256::default(), 0)));
+        }
         
         let blockchain_db = Self {
             db: db,
+            proposer_best: Mutex::new((H256::default(), 0)),
+            voter_best: voter_best,
         };
         
         return Ok(blockchain_db);
@@ -116,11 +122,16 @@ impl BlockChain {
 
         // insert genesis blocks
         let mut wb = WriteBatch::default();
+
         // proposer genesis block
         wb.put_cf(proposer_node_level_cf, serialize(&(*PROPOSER_GENESIS_HASH)).unwrap(), 
                   serialize(&(0 as u64)).unwrap())?;
         wb.merge_cf(proposer_tree_level_cf, serialize(&(0 as u64)).unwrap(),
                     serialize(&(*PROPOSER_GENESIS_HASH)).unwrap())?;
+        let mut proposer_best = db.proposer_best.lock().unwrap();
+        proposer_best.0 = *PROPOSER_GENESIS_HASH;
+        drop(proposer_best);
+
         // voter genesis blocks
         for chain_num in 0..NUM_VOTER_CHAINS {
             wb.put_cf(parent_neighbor_cf, serialize(&VOTER_GENESIS_HASHES[chain_num as usize]).unwrap(), 
@@ -135,6 +146,9 @@ impl BlockChain {
                       serialize(&(0 as u64)).unwrap())?;
             wb.put_cf(voter_node_chain_cf, serialize(&VOTER_GENESIS_HASHES[chain_num as usize]).unwrap(), 
                       serialize(&(chain_num as u16)).unwrap())?;
+            let mut voter_best = db.voter_best[chain_num as usize].lock().unwrap();
+            voter_best.0 = VOTER_GENESIS_HASHES[chain_num as usize];
+            drop(voter_best);
         }
         db.db.write(wb)?;
 
@@ -181,6 +195,13 @@ impl BlockChain {
                           serialize(&self_level).unwrap())?;
                 wb.merge_cf(proposer_tree_level_cf, serialize(&self_level).unwrap(),
                             serialize(&block_hash).unwrap())?;
+                // set best block info
+                let mut proposer_best = self.proposer_best.lock().unwrap();
+                if self_level > proposer_best.1 {
+                    proposer_best.0 = block_hash;
+                    proposer_best.1 = self_level;
+                }
+                drop(proposer_best);
             }
             Content::Voter(content) => {
                 // add voter parent
@@ -217,6 +238,13 @@ impl BlockChain {
                 }
                 wb.put_cf(voter_node_voted_level_cf, serialize(&block_hash).unwrap(), 
                           serialize(&deepest_voted_level).unwrap())?;
+                // set best block info
+                let mut voter_best = self.voter_best[self_chain as usize].lock().unwrap();
+                if self_level > voter_best.1 {
+                    voter_best.0 = block_hash;
+                    voter_best.1 = self_level;
+                }
+                drop(voter_best);
             }
             Content::Transaction(_) => {
             }
@@ -267,6 +295,7 @@ mod tests {
         assert_eq!(genesis_level, 0);
         let level_0_blocks: Vec<H256> = deserialize(&db.db.get_cf(proposer_tree_level_cf, serialize(&(0 as u64)).unwrap()).unwrap().unwrap()).unwrap();
         assert_eq!(level_0_blocks, vec![*PROPOSER_GENESIS_HASH]);
+        assert_eq!(*db.proposer_best.lock().unwrap(), (*PROPOSER_GENESIS_HASH, 0));
 
         // validate voter genesis
         for chain_num in 0..NUM_VOTER_CHAINS {
@@ -280,6 +309,7 @@ mod tests {
             assert_eq!(parent, *PROPOSER_GENESIS_HASH);
             let voted_proposer: Vec<H256> = deserialize(&db.db.get_cf(vote_neighbor_cf, serialize(&VOTER_GENESIS_HASHES[chain_num as usize]).unwrap()).unwrap().unwrap()).unwrap();
             assert_eq!(voted_proposer, vec![*PROPOSER_GENESIS_HASH]);
+            assert_eq!(*db.voter_best[chain_num as usize].lock().unwrap(), (VOTER_GENESIS_HASHES[chain_num as usize], 0));
         }
         let voters: Vec<H256> = deserialize(&db.db.get_cf(vote_neighbor_cf, serialize(&(*PROPOSER_GENESIS_HASH)).unwrap()).unwrap().unwrap()).unwrap();
         assert_eq!(voters, *VOTER_GENESIS_HASHES);
@@ -357,6 +387,7 @@ mod tests {
         assert_eq!(proposer_ref, vec![new_proposer_block_1.hash()]);
         let transaction_ref: Vec<H256> = deserialize(&db.db.get_cf(transaction_ref_neighbor_cf, serialize(&new_proposer_block_2.hash()).unwrap()).unwrap().unwrap()).unwrap();
         assert_eq!(transaction_ref, vec![new_transaction_block.hash()]);
+        assert_eq!(*db.proposer_best.lock().unwrap(), (new_proposer_block_1.hash(), 1));
 
         // Create a voter block attached to proposer block 2 and the first voter chain, and vote for proposer block 1.
         let new_voter_content = Content::Voter(voter::Content::new(1, VOTER_GENESIS_HASHES[0], vec![new_proposer_block_1.hash()]));
@@ -384,6 +415,7 @@ mod tests {
         assert_eq!(voted, vec![new_proposer_block_1.hash()]);
         let voter: Vec<H256> = deserialize(&db.db.get_cf(vote_neighbor_cf, serialize(&new_proposer_block_1.hash()).unwrap()).unwrap().unwrap()).unwrap();
         assert_eq!(voter, vec![new_voter_block.hash()]);
+        assert_eq!(*db.voter_best[0].lock().unwrap(), (new_voter_block.hash(), 1));
 
     }
 
