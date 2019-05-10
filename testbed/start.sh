e5221f8f595cf0222dc7bde3ac3681c772e01877
Lei Yang <lei@yangl1996.com>
2019-05-10 11:53:23 -0400

    let the script to try to figure out binary path

--- testbed/start.sh
+++ testbed/start.sh
@@ -5,12 +5,12 @@ if [ "$#" -lt "2" ]; then
 	exit 0
 fi
 
+binary_path=${PRISM_BINARY-../target/debug/prism}
 node_name=$1
 node_port=$2
 shift 2	# pop the first arg
-binary_path=$PRISM_BINARY
 
-command="$PRISM_BINARY --port ${node_port} --blockdb /tmp/${node_name}-blockdb.rocksdb --blockchain /tmp/${node_name}-blockchain.rocksdb --utxodb /tmp/${node_name}-utxodb.rocksdb --wallet /tmp/${node_name}-wallet.rocksdb --mine -vv"
+command="$binary_path --port ${node_port} --blockdb /tmp/${node_name}-blockdb.rocksdb --blockchain /tmp/${node_name}-blockchain.rocksdb --utxodb /tmp/${node_name}-utxodb.rocksdb --wallet /tmp/${node_name}-wallet.rocksdb --mine -vv"
 
 for port in "$@"; do
 	command="$command -c 127.0.0.1:${port}"
