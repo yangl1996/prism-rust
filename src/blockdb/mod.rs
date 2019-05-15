bebf65254a03fd908f6bf356a454511d4f20b70a
Lei Yang <lei@yangl1996.com>
2019-05-14 23:05:17 -0400

    record insertion sequence of blocks

--- src/blockdb/mod.rs
+++ src/blockdb/mod.rs
@@ -4,26 +4,33 @@ use crate::block::Block;
 use crate::config::*;
 use crate::crypto::hash::{Hashable, H256};
 use bincode::{deserialize, serialize};
-use rocksdb::{self, Options, DB};
-use std::sync::atomic::{AtomicUsize, Ordering};
+use rocksdb::{self, Options, DB, ColumnFamilyDescriptor};
+use std::sync::atomic::{AtomicU64, Ordering};
+
+const BLOCK_CF: &str = "BLOCK";
+const BLOCK_ARRIVAL_ORDER_CF: &str = "BLOCK_ARRIVAL_ORDER";
 
 /// Database that stores blocks.
 pub struct BlockDatabase {
     /// The underlying RocksDB handle.
-    handle: rocksdb::DB,
+    db: rocksdb::DB,
     /// The number of blocks in this database.
-    count: AtomicUsize,
+    count: AtomicU64,
 }
 
 impl BlockDatabase {
     /// Open the database at the given path, and create a new one if missing.
     fn open<P: AsRef<std::path::Path>>(path: P) -> Result<Self, rocksdb::Error> {
+        let block_cf = ColumnFamilyDescriptor::new(BLOCK_CF, Options::default());
+        let block_arrival_order_cf = ColumnFamilyDescriptor::new(BLOCK_ARRIVAL_ORDER_CF, Options::default());
+        let cfs = vec![block_cf, block_arrival_order_cf];
         let mut opts = Options::default();
         opts.create_if_missing(true);
-        let db = DB::open(&opts, path)?;
+        opts.create_missing_column_families(true);
+        let db = DB::open_cf_descriptors(&opts, path, cfs)?;
         return Ok(BlockDatabase {
-            handle: db,
-            count: AtomicUsize::new(0),
+            db: db,
+            count: AtomicU64::new(0),
         });
     }
 
@@ -32,23 +39,41 @@ impl BlockDatabase {
         DB::destroy(&Options::default(), &path)?;
         let db = Self::open(&path)?;
 
+        let block_cf = db.db.cf_handle(BLOCK_CF).unwrap();
+        let block_arrival_order_cf = db.db.cf_handle(BLOCK_ARRIVAL_ORDER_CF).unwrap();
+
+        let mut counter: u64 = 0;
         // insert proposer genesis block
         let proposer_genesis_hash_u8: [u8; 32] = (*PROPOSER_GENESIS_HASH).into();
-        db.handle.put(
+        db.db.put_cf(
+            block_cf,
             &(*PROPOSER_GENESIS_HASH),
             &serialize(&proposer_genesis()).unwrap(),
         )?;
+        db.db.put_cf(
+            block_arrival_order_cf,
+            &counter.to_ne_bytes(),
+            &(*PROPOSER_GENESIS_HASH)
+        )?;
+        counter += 1;
 
         // insert voter genesis blocks
         for i in 0..NUM_VOTER_CHAINS {
-            db.handle.put(
+            db.db.put_cf(
+                block_cf,
                 &VOTER_GENESIS_HASHES[i as usize],
                 &serialize(&voter_genesis(i as u16)).unwrap(),
             )?;
+            db.db.put_cf(
+                block_arrival_order_cf,
+                &counter.to_ne_bytes(),
+                &VOTER_GENESIS_HASHES[i as usize],
+                )?;
+            counter += 1;
         }
 
         db.count
-            .store(1 + NUM_VOTER_CHAINS as usize, Ordering::Relaxed);
+            .store(counter, Ordering::Relaxed);
         return Ok(db);
     }
 
@@ -58,32 +83,31 @@ impl BlockDatabase {
         return Ok(db);
     }
 
-    /// Insert a new block to the database.
-    pub fn insert(&self, block: &Block) -> Result<(), rocksdb::Error> {
+    /// Insert a new block to the database and returns the sequence number of the block.
+    pub fn insert(&self, block: &Block) -> Result<u64, rocksdb::Error> {
+        let block_cf = self.db.cf_handle(BLOCK_CF).unwrap();
+        let block_arrival_order_cf = self.db.cf_handle(BLOCK_ARRIVAL_ORDER_CF).unwrap();
         let hash: H256 = block.hash();
         let serialized = serialize(block).unwrap();
-        self.count.fetch_add(1, Ordering::Relaxed);
-        return self.handle.put(&hash, &serialized);
+        let counter = self.count.fetch_add(1, Ordering::Relaxed);
+        self.db.put_cf(block_cf, &hash, &serialized)?;
+        self.db.put_cf(block_arrival_order_cf, &counter.to_ne_bytes(), &hash)?;
+        return Ok(counter);
     }
 
     /// Get a block from the database.
     pub fn get(&self, hash: &H256) -> Result<Option<Block>, rocksdb::Error> {
-        let serialized = self.handle.get(hash)?;
+        let block_cf = self.db.cf_handle(BLOCK_CF).unwrap();
+        let block_arrival_order_cf = self.db.cf_handle(BLOCK_ARRIVAL_ORDER_CF).unwrap();
+        let serialized = self.db.get_cf(block_cf, hash)?;
         match serialized {
             None => return Ok(None),
             Some(s) => return Ok(Some(deserialize(&s).unwrap())),
         }
     }
 
-    /// Delete a block from the database
-    // TODO: what if the block does not exist?
-    pub fn delete(&self, hash: &H256) -> Result<(), rocksdb::Error> {
-        self.count.fetch_sub(1, Ordering::Relaxed);
-        return self.handle.delete(hash);
-    }
-
     /// Get the number of blocks in the database.
-    pub fn num_blocks(&self) -> usize {
+    pub fn num_blocks(&self) -> u64 {
         let count = self.count.load(Ordering::Relaxed);
         return count;
     }
@@ -95,20 +119,17 @@ mod tests {
     use crate::crypto::hash::Hashable;
 
     #[test]
-    fn insert_get_and_delete() {
+    fn insert_and_get() {
         let db = BlockDatabase::new(&std::path::Path::new(
             "/tmp/blockdb_tests_insert_get_and_delete.rocksdb",
         ))
         .unwrap();
         let block = proposer_genesis();
-        db.insert(&block).unwrap();
+        let seq = db.insert(&block).unwrap();
         let got = db.get(&block.hash()).unwrap().unwrap();
         let num_block = db.num_blocks();
         assert_eq!(got.hash(), block.hash());
-        assert_eq!(num_block, 1 + NUM_VOTER_CHAINS as usize + 1);
-        db.delete(&block.hash()).unwrap();
-        let num_block = db.num_blocks();
-        assert_eq!(db.get(&block.hash()).unwrap().is_none(), true);
-        assert_eq!(num_block, 1 + NUM_VOTER_CHAINS as usize);
+        assert_eq!(num_block, 1 + NUM_VOTER_CHAINS as u64 + 1);
+        assert_eq!(seq, num_block - 1);
     }
 }
