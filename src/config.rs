5ded1ab073ed5b1c29aee9754700920c2471d3cc
Vivek Bagaria <vivekee047@gmail.com>
2019-05-19 18:43:02 -0700

    All the constants in the config file soley depend on the network parameters and 3 design parameters

--- src/config.rs
+++ src/config.rs
@@ -1,32 +1,44 @@
 use crate::crypto::hash::H256;
 
-// Block sizes
-pub const TX_BLOCK_SIZE: usize = 10;
-pub const PROPOSER_BLOCK_SIZE: usize = 10;
-pub const VOTER_BLOCK_SIZE: usize = 10;
+// Network parameters
+pub const NETWORK_CAPACITY: u32 = 500_000; // 0.5 MB/s == 4Mb/s. We will use 80% of capacity for the txs.
+pub const NETWORK_DELAY: f32 = 2.0; // 2 sec delay
+// Design parameters
+pub const NUM_VOTER_CHAINS: u16 = 100 as u16; //
+pub const TX_BLOCK_SIZE_BYTES: u32 = 64_000; //64KB
+pub const AVG_TX_SIZE_BYTES: u32 = 500;
 
-// chain id
+
+// All the parameters below are function of the above parameters
+pub const TX_THROUGHPUT: u32 = NETWORK_CAPACITY*4/5; // .4 MB == 3.2 Mb.
+pub const CHAIN_MINING_RATE: f32 = 0.2/(NETWORK_DELAY); // Mining rate of each chain.
+
+
+//Ratio Prop::Total_Voter::Tx_block
+pub const RATIO: (f32, f32, f32) = (1.0, NUM_VOTER_CHAINS as f32, (TX_THROUGHPUT as f32)/((TX_BLOCK_SIZE_BYTES as f32)*CHAIN_MINING_RATE) ); 
+
+// Mining rates in percentages*100 of the total mining rate
+pub const TOTAL_MINING_RANGE: u32 = 10000; // This is only used for resolution
+pub const CHAIN_MINING_RANGE: u32 = ((TOTAL_MINING_RANGE as f32)/(RATIO.0+RATIO.1+RATIO.2)) as u32;
+// Total for the voter chains
+pub const VOTER_MINING_RANGE: u32 = CHAIN_MINING_RANGE * (NUM_VOTER_CHAINS as u32);
+// Proposer tree
+pub const PROPOSER_MINING_RANGE: u32 = CHAIN_MINING_RANGE;
+// Transaction blocks
+pub const TRANSACTION_MINING_RANGE: u32 = TOTAL_MINING_RANGE - PROPOSER_MINING_RANGE - VOTER_MINING_RANGE;
+
+//Block content size limits
+pub const TRANSACTION_BLOCK_TX_LIMIT: u32 = TX_BLOCK_SIZE_BYTES/AVG_TX_SIZE_BYTES; // Max number of tx included in a tx block
+pub const PROPOSER_BLOCK_TX_BLOCK_REF_LIMIT: u32 = 3*TRANSACTION_MINING_RANGE/PROPOSER_MINING_RANGE; // Max number of tx blocks referred by a prop block.
+pub const PROPOSER_BLOCK_PROP_BLOCK_REF_LIMIT: u32 = 10; // Max number of prop blocks referred by a prop block.
+
+
+
+// Chain id
 pub const TRANSACTION_INDEX: u32 = 1;
 pub const PROPOSER_INDEX: u32 = 0;
 pub const FIRST_VOTER_INDEX: u32 = 2;
 
-// 1:100:100 - Ratio
-// Number of voter chains
-pub const NUM_VOTER_CHAINS: u16 = 100;
-
-// Mining rates in percentages of the total mining rate
-pub const CHAIN_MINING_RATE: u32 = 50;
-// Total for the voter chains
-pub const VOTER_MINING_RATE: u32 = CHAIN_MINING_RATE * (NUM_VOTER_CHAINS as u32);
-// Proposer tree
-pub const PROPOSER_MINING_RATE: u32 = CHAIN_MINING_RATE;
-// Transaction blocks
-pub const TRANSACTION_MINING_RATE: u32 = 10000 - PROPOSER_MINING_RATE - VOTER_MINING_RATE;
-
-//Block content limits
-pub const TRANSACTION_BLOCK_TX_LIMIT: u32 = 400; //Max number of tx included in a tx block
-pub const PROPOSER_BLOCK_TX_BLOCK_REF_LIMIT: u32 = 300; // Max number of tx blocks referred by a prop block.
-pub const PROPOSER_BLOCK_PROP_BLOCK_REF_LIMIT: u32 = 10; // Max number of prop blocks referred by a prop block.
 
 lazy_static! {
     pub static ref DEFAULT_DIFFICULTY: H256 = {
@@ -54,4 +66,10 @@ lazy_static! {
         }
         v
     };
+    // Max number of votes cast by a voter block.
+    pub static ref VOTER_BLOCK_VOTES_LIMIT: u32 = {
+        let log_no_voter_chains = (NUM_VOTER_CHAINS as f64).ln();
+        3*(log_no_voter_chains.ceil() as u32)
+    };
+
 }
--- src/miner/mod.rs
+++ src/miner/mod.rs
@@ -292,7 +292,7 @@ impl Context {
             .collect();
         // get mutex of mempool and get all required data
         let mempool = self.tx_mempool.lock().unwrap();
-        let transactions = mempool.get_transactions(TX_BLOCK_SIZE);
+        let transactions = mempool.get_transactions(TRANSACTION_BLOCK_TX_LIMIT);
         drop(mempool);
 
         // update the contents and the parents based on current view
@@ -335,20 +335,22 @@ impl Context {
         let big_hash = U256::from_big_endian(hash);
         let difficulty_raw: [u8; 32] = (&self.difficulty).into();
         let big_difficulty = U256::from_big_endian(&difficulty_raw);
-        let big_proposer_rate: U256 = PROPOSER_MINING_RATE.into();
-        let big_transaction_rate: U256 = TRANSACTION_MINING_RATE.into();
-        if big_hash < big_difficulty / 10000.into() * big_proposer_rate {
+        let total_mining_range: U256 = TOTAL_MINING_RANGE.into();
+        let big_proposer_range: U256 = PROPOSER_MINING_RANGE.into();
+        let big_transaction_range: U256 = TRANSACTION_MINING_RANGE.into();
+
+        if big_hash < big_difficulty / total_mining_range * big_proposer_range {
             // proposer block
             return PROPOSER_INDEX;
         } else if big_hash
-            < big_difficulty / 10000.into() * (big_transaction_rate + big_proposer_rate)
+            < big_difficulty / total_mining_range * (big_transaction_range + big_proposer_range)
         {
             // transaction block
             return TRANSACTION_INDEX;
         } else if big_hash < big_difficulty {
             // voter index, figure out which voter tree we are in
             let voter_id =
-                (big_hash - big_transaction_rate - big_proposer_rate) % NUM_VOTER_CHAINS.into();
+                (big_hash - big_transaction_range - big_proposer_range) % NUM_VOTER_CHAINS.into();
             return voter_id.as_u32() + FIRST_VOTER_INDEX;
         } else {
             panic!(
@@ -483,8 +485,8 @@ mod tests {
         let big_difficulty = U256::from_big_endian(&DEFAULT_DIFFICULTY);
 
         let mut big_hash: U256;
-        let big_proposer_rate: U256 = PROPOSER_MINING_RATE.into();
-        let big_transaction_rate: U256 = TRANSACTION_MINING_RATE.into();
+        let big_proposer_range: U256 = PROPOSER_MINING_RANGE.into();
+        let big_transaction_range: U256 = TRANSACTION_MINING_RANGE.into();
 
         let mut hash: [u8; 32];
         let mut sortition_id: u32;
@@ -495,28 +497,28 @@ mod tests {
         assert_eq!(sortition_id, PROPOSER_INDEX);
 
         // Set the hash to just below the boundary between tx and proposer blocks
-        big_hash = big_difficulty / 100.into() * big_proposer_rate - 1.into();
+        big_hash = big_difficulty / 100.into() * big_proposer_range - 1.into();
         hash = big_hash.into();
         sortition_id = ctx.get_sortition_id(&hash);
         assert_eq!(sortition_id, PROPOSER_INDEX);
 
         // Proposer blocks
         // Set the hash to just above the boundary between tx and proposer blocks
-        big_hash = big_difficulty / 100.into() * big_proposer_rate;
+        big_hash = big_difficulty / 100.into() * big_proposer_range;
         hash = big_hash.into();
         sortition_id = ctx.get_sortition_id(&hash);
         assert_eq!(sortition_id, TRANSACTION_INDEX);
 
         // Set the hash to just below the boundary between tx and voter blocks
         big_hash =
-            big_difficulty / 100.into() * (big_transaction_rate + big_proposer_rate) - 1.into();
+            big_difficulty / 100.into() * (big_transaction_range + big_proposer_range) - 1.into();
         hash = big_hash.into();
         sortition_id = ctx.get_sortition_id(&hash);
         assert_eq!(sortition_id, TRANSACTION_INDEX);
 
         // Voter blocks
         // Set the hash to just above the boundary between tx and voter blocks
-        big_hash = big_difficulty / 100.into() * (big_transaction_rate + big_proposer_rate);
+        big_hash = big_difficulty / 100.into() * (big_transaction_range + big_proposer_range);
         hash = big_hash.into();
         sortition_id = ctx.get_sortition_id(&hash);
         assert_eq!(sortition_id, FIRST_VOTER_INDEX);
