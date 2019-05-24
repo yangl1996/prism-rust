6b115ae30c464f4c6bebcfef0dad3b2ba635eef7
Lei Yang <lei@yangl1996.com>
2019-05-24 10:14:22 -0400

    update the start script to accept api port

--- testbed/start.sh
+++ testbed/start.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-if [ "$#" -lt "3" ]; then
-	echo "Usage: ./start.sh <node name> <node port> <visual port> <peer port> ..."
+if [ "$#" -lt "4" ]; then
+	echo "Usage: ./start.sh <node name> <node port> <api port> <visual port> <peer port> ..."
 	exit 0
 fi
 
 binary_path=${PRISM_BINARY-../target/debug/prism}
 node_name=$1
 node_port=$2
-visual_port=$3
-shift 3	# pop the first arg
+api_port=$3
+visual_port=$4
+shift 4	# pop the first arg
 
-command="$binary_path --p2p 127.0.0.1:${node_port} --blockdb /tmp/${node_name}-blockdb.rocksdb --blockchaindb /tmp/${node_name}-blockchain.rocksdb --utxodb /tmp/${node_name}-utxodb.rocksdb --walletdb /tmp/${node_name}-wallet.rocksdb --mine -vvvv --visual 127.0.0.1:${visual_port}"
+command="$binary_path --p2p 127.0.0.1:${node_port} --api 127.0.0.1:${api_port} --blockdb /tmp/${node_name}-blockdb.rocksdb --blockchaindb /tmp/${node_name}-blockchain.rocksdb --utxodb /tmp/${node_name}-utxodb.rocksdb --walletdb /tmp/${node_name}-wallet.rocksdb --mine -vvvv --visual 127.0.0.1:${visual_port}"
 
 for port in "$@"; do
 	command="$command -c 127.0.0.1:${port}"
@@ -19,5 +20,3 @@ done
 
 eval $command
 
-#Example bash start.sh miner1 10001 127.0.0.1:8001
-#        bash start.sh miner2 10002 127.0.0.1:8002 10001
