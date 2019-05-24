0501eb8d8d483968af9b62a1740ffbefc85c2b4c
Lei Yang <lei@yangl1996.com>
2019-05-24 10:06:24 -0400

    update start script to support the new cli api

--- testbed/start.sh
+++ testbed/start.sh
@@ -11,7 +11,7 @@ node_port=$2
 visual_port=$3
 shift 3	# pop the first arg
 
-command="$binary_path --port ${node_port} --blockdb /tmp/${node_name}-blockdb.rocksdb --blockchaindb /tmp/${node_name}-blockchain.rocksdb --utxodb /tmp/${node_name}-utxodb.rocksdb --walletdb /tmp/${node_name}-wallet.rocksdb --mine -vvvv --visual ${visual_port}"
+command="$binary_path --p2p 127.0.0.1:${node_port} --blockdb /tmp/${node_name}-blockdb.rocksdb --blockchaindb /tmp/${node_name}-blockchain.rocksdb --utxodb /tmp/${node_name}-utxodb.rocksdb --walletdb /tmp/${node_name}-wallet.rocksdb --mine -vvvv --visual 127.0.0.1:${visual_port}"
 
 for port in "$@"; do
 	command="$command -c 127.0.0.1:${port}"
