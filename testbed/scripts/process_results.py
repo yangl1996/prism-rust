e20c5bc3e5b1d2e8ef835fdf9b7d7cd2774f2456
Lei Yang <lei@yangl1996.com>
2019-05-28 22:01:29 -0400

    track total mined blocks

--- testbed/scripts/process_results.py
+++ testbed/scripts/process_results.py
@@ -62,7 +62,7 @@ print_node_results("Transaction generation", [('generated_transactions', 'Txs'),
 print_node_results("Transaction Block confirmation", [('confirmed_transaction_blocks', 'Blks')], 'average')
 print_node_results("Transaction confirmation", [('confirmed_transactions', 'Txs'), ('confirmed_transaction_bytes', 'B')], 'average')
 print_node_results("Block processed (proposer, voter, transaction)", [('processed_proposer_blocks', 'Blks'), ('processed_voter_blocks', 'Blks'), ('processed_transaction_blocks', 'Blks')], 'average')
-print_node_results("Block mined (proposer, voter, transaction)", [('mined_proposer_blocks', 'Blks'), ('mined_voter_blocks', 'Blks'), ('mined_transaction_blocks', 'Blks')], 'average')
+print_node_results("Block mined (proposer, voter, transaction)", [('mined_proposer_blocks', 'Blks'), ('mined_voter_blocks', 'Blks'), ('mined_transaction_blocks', 'Blks')], 'sum')
 
 print("Transaction generation failures: {:.2f} Txs".format(tx_gen_fails))
 
