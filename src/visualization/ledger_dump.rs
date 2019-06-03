f8c50c018c19d9e07c503824e8e6e3f85519ccf6
Lei Yang <lei@yangl1996.com>
2019-06-03 10:35:10 -0700

    use /* */ to comment

--- src/visualization/ledger_dump.rs
+++ src/visualization/ledger_dump.rs
@@ -64,63 +64,65 @@ pub fn dump_ledger(
     let mut proposer_blocks: Vec<String> = vec![];
     // loop over all tx blocks in the ledger
     for (proposer_hash, tx_block_hashes) in &ledger {
-//        let mut transactions_blocks: Vec<TransactionBlock> = vec![];
-//        for tx_block_hash in tx_block_hashes {
-//            let mut transactions = vec![];
-//            let mut utxos = vec![];
-//            let transactions_in_block: Vec<RawTransaction> = match blockdb.get(tx_block_hash) {
-//                Err(_) => return "database err".to_string(),
-//                Ok(None) => return "transaction block not found".to_string(),
-//                Ok(Some(block)) => match block.content {
-//                    Content::Transaction(content) => content.transactions,
-//                    _ => return "wrong block type, not transaction block".to_string(),
-//                },
-//            };
-//
-//            // loop over all the tx in this transaction block
-//            for tx in transactions_in_block {
-//                let hash: H256 = tx.hash();
-//                // loop over the outputs to check if they are unspent
-//                for index in 0..tx.output.len() {
-//                    let coin_id = CoinId {
-//                        hash,
-//                        index: index as u32,
-//                    };
-//                    if let Ok(true) = utxodb.contains(&coin_id) {
-//                        utxos.push(Input {
-//                            hash: hash.to_string(),
-//                            index: index as u32,
-//                        });
-//                    }
-//                }
-//
-//                // add this transaction to the list
-//                transactions.push(Transaction {
-//                    hash: hash.to_string(),
-//                    input: tx
-//                        .input
-//                        .iter()
-//                        .map(|x| Input {
-//                            hash: x.coin.hash.to_string(),
-//                            index: x.coin.index,
-//                        })
-//                        .collect(),
-//                    output: tx
-//                        .output
-//                        .iter()
-//                        .map(|x| Output {
-//                            value: x.value,
-//                            recipient: x.recipient.to_string(),
-//                        })
-//                        .collect(),
-//                });
-//            }
-//            transactions_blocks.push(TransactionBlock {
-//                hash: tx_block_hash.to_string(),
-//                transactions,
-//                utxos,
-//            });
-//        }
+        /*
+        let mut transactions_blocks: Vec<TransactionBlock> = vec![];
+        for tx_block_hash in tx_block_hashes {
+            let mut transactions = vec![];
+            let mut utxos = vec![];
+            let transactions_in_block: Vec<RawTransaction> = match blockdb.get(tx_block_hash) {
+                Err(_) => return "database err".to_string(),
+                Ok(None) => return "transaction block not found".to_string(),
+                Ok(Some(block)) => match block.content {
+                    Content::Transaction(content) => content.transactions,
+                    _ => return "wrong block type, not transaction block".to_string(),
+                },
+            };
+
+            // loop over all the tx in this transaction block
+            for tx in transactions_in_block {
+                let hash: H256 = tx.hash();
+                // loop over the outputs to check if they are unspent
+                for index in 0..tx.output.len() {
+                    let coin_id = CoinId {
+                        hash,
+                        index: index as u32,
+                    };
+                    if let Ok(true) = utxodb.contains(&coin_id) {
+                        utxos.push(Input {
+                            hash: hash.to_string(),
+                            index: index as u32,
+                        });
+                    }
+                }
+
+                // add this transaction to the list
+                transactions.push(Transaction {
+                    hash: hash.to_string(),
+                    input: tx
+                        .input
+                        .iter()
+                        .map(|x| Input {
+                            hash: x.coin.hash.to_string(),
+                            index: x.coin.index,
+                        })
+                        .collect(),
+                    output: tx
+                        .output
+                        .iter()
+                        .map(|x| Output {
+                            value: x.value,
+                            recipient: x.recipient.to_string(),
+                        })
+                        .collect(),
+                });
+            }
+            transactions_blocks.push(TransactionBlock {
+                hash: tx_block_hash.to_string(),
+                transactions,
+                utxos,
+            });
+        }
+        */
         proposer_blocks.push(proposer_hash.to_string());
     }
     let dump = Dump {
