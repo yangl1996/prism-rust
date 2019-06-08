1b39b497b83e434b8054949160661e675dc03329
Lei Yang <lei@yangl1996.com>
2019-06-07 17:20:47 -0700

    ask for blocks from only one peer

--- src/network/worker.rs
+++ src/network/worker.rs
@@ -199,7 +199,7 @@ impl Context {
                     if !to_request.is_empty() {
                         to_request.sort();
                         to_request.dedup();
-                        self.server.broadcast(Message::GetBlocks(to_request));
+                        peer.write(Message::GetBlocks(to_request));
                     }
 
                     // tell the miner to update the context
