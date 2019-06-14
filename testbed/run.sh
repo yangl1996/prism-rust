7d474722baac3c3b3960e31c3623cfa133d6a46b
Lei Yang <lei@yangl1996.com>
2019-06-14 12:00:48 -0400

    no need to average window size

--- testbed/run.sh
+++ testbed/run.sh
@@ -217,7 +217,7 @@ function start_transactions_single
 	curl -s "http://$3:$4/transaction-generator/set-arrival-distribution?interval=1000&distribution=uniform"
 	curl -s "http://$3:$4/transaction-generator/set-value-distribution?min=100&max=100&distribution=uniform"
 	curl -s "http://$3:$4/transaction-generator/start?throttle=500000"
-	curl -s "http://$3:$4/miner/start?lambda=20000&lazy=false"
+	curl -s "http://$3:$4/miner/start?lambda=200000&lazy=false"
 }
 
 function query_api 
--- testbed/telematics/main.go
+++ testbed/telematics/main.go
@@ -170,7 +170,7 @@ func log(interval, duration uint, nodesFile, dataDir string) {
 				tm.Printf("  Generated Transactions    %8v  %8v\n", cavg.Generated_transactions / dur, (cavg.Generated_transactions - pavg.Generated_transactions) / int(interval))
 				tm.Printf("  Confirmed Transactions    %8v  %8v\n", cavg.Confirmed_transactions / dur, (cavg.Confirmed_transactions - pavg.Confirmed_transactions) / int(interval))
 				tm.Printf("Deconfirmed Transactions    %8v  %8v\n", cavg.Deconfirmed_transactions / dur, (cavg.Deconfirmed_transactions - pavg.Deconfirmed_transactions) / int(interval))
-				tm.Printf("            Queue Length    %8v  %8v\n", cavg.Incoming_message_queue / dur, (cavg.Incoming_message_queue - pavg.Incoming_message_queue) / int(interval))
+				tm.Printf("            Queue Length    %8v  %8v\n", cavg.Incoming_message_queue, (cavg.Incoming_message_queue - pavg.Incoming_message_queue) / int(interval))
 				tm.Printf("    Mining -    Proposer    %8.3g  %8.3g\n", float64(cavg.Mined_proposer_blocks) / float64(dur), float64(cavg.Mined_proposer_blocks - pavg.Mined_proposer_blocks) / float64(interval))
 				tm.Printf("    Mining -       Voter    %8.3g  %8.3g\n", float64(cavg.Mined_voter_blocks) / float64(dur), float64(cavg.Mined_voter_blocks - pavg.Mined_voter_blocks) / float64(interval))
 				tm.Printf("    Mining - Transaction    %8.3g  %8.3g\n", float64(cavg.Mined_transaction_blocks) / float64(dur), float64(cavg.Mined_transaction_blocks - pavg.Mined_transaction_blocks) / float64(interval))
