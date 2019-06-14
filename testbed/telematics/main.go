e5e2c97d6ba43e84b81a6aca3fd3f812d5bf6613
Lei Yang <lei@yangl1996.com>
2019-06-13 20:28:51 -0400

    read node list

--- testbed/telematics/main.go
+++ testbed/telematics/main.go
@@ -5,14 +5,51 @@ import (
 	"fmt"
 	"time"
 	"os"
+	"bufio"
+	"strings"
 )
 
 func main() {
-	c := rrd.NewCreator("data.rrd", time.Now(), 10)
-	c.DS("confirmed_tx", "COUNTER", 20, 0, "U")
-	err := c.Create(true)
+	fmt.Println("Parsing node list")
+	nodes := make(map[string]string)
+	file, err := os.Open("nodes.txt")
 	if err != nil {
-		fmt.Println("Error creating round-robin database:", err)
+		fmt.Println("Error opening node list:", err)
 		os.Exit(1)
 	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), ",")
+		name := s[0]
+		ip := s[2]
+		port := s[5]
+		url := fmt.Sprintf("http://%v:%v/telematics/snapshot", ip, port)
+		nodes[name] = url
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Println("Error reading node list:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Creating round-robin database")
+	err = os.MkdirAll("data", os.ModeDir)
+	if err != nil {
+		fmt.Println("Error creating data directory", err)
+		os.Exit(1)
+	}
+	for k, _ := range nodes {
+		n := "data/" + k + ".rrd"
+		c := rrd.NewCreator(n, time.Now(), 10)
+		c.DS("confirmed_tx", "COUNTER", 20, 0, "U")
+		c.DS("deconfirmed_tx", "COUNTER", 20, 0, "U")
+		c.DS("generated_tx", "COUNTER", 20, 0, "U")
+		c.RRA("LAST", 0, 1, 4320)	// collect 4320 datapoints (12 hours)
+		err = c.Create(true)
+		if err != nil {
+			fmt.Println("Error creating round-robin database:", err)
+			os.Exit(1)
+		}
+	}
+
 }
