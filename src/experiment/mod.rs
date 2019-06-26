7306f71c6c1acfa0879bc452bbbfb1f5fdbcc4f9
Lei Yang <lei@yangl1996.com>
2019-06-26 15:13:40 -0400

    use multiple threads to write initial coins

--- src/experiment/mod.rs
+++ src/experiment/mod.rs
@@ -6,32 +6,50 @@ use crate::utxodb::UtxoDatabase;
 use crate::transaction::{CoinId, Input, Output, Transaction};
 use crate::wallet::Wallet;
 use crate::crypto::hash::{H256, Hashable};
+use std::sync::{Arc, Mutex};
+use std::thread;
 
 pub fn ico(
     recipients: &[H256], // addresses of all the ico recipients
-    utxodb: &UtxoDatabase,
-    wallet: &Wallet,
+    utxodb: &Arc<UtxoDatabase>,
+    wallet: &Arc<Wallet>,
     num_coins: usize,
     value: u64,
 ) -> Result<(), rocksdb::Error> {
-    let funding = Transaction {
-        input: vec![],
-        output: recipients
-            .iter()
-            .map(|recipient| {
-                (0..num_coins).map(move |_| Output {
-                    value: value,
-                    recipient: recipient.clone(),
-                })
-            })
-            .flatten()
-            .collect(),
-        authorization: vec![],
-        hash: RefCell::new(None)
-    };
-    let hash = funding.hash();
-    let diff = utxodb.apply_diff(&[(funding, hash)], &[]).unwrap();
+    let recipients: Vec<H256> = recipients.to_vec();
+    let recipients = Arc::new(Mutex::new(recipients));
+
+    // start a bunch of worker threads to commit those coins
+    let mut workers = vec![];
+    for _ in 0..8 {
+        let recipients = Arc::clone(&recipients);
+        let utxodb = Arc::clone(&utxodb);
+        let wallet = Arc::clone(&wallet);
+        let handle = thread::spawn(move || {
+            loop {
+                let recipient = match recipients.lock().unwrap().pop() {
+                    Some(r) => r,
+                    None => break,
+                };
+                let tx = Transaction {
+                    input: vec![],
+                    output: (0..num_coins).map(move |_| Output {
+                        value: value, 
+                        recipient: recipient
+                    }).collect(),
+                    authorization: vec![],
+                    hash: RefCell::new(None),
+                };
+                let hash = tx.hash();
+                let diff = utxodb.apply_diff(&[(tx, hash)], &[]).unwrap();
+                wallet.apply_diff(&diff.0, &diff.1).unwrap();
+            }
+        });
+        workers.push(handle);
+    }
+    for child in workers.drain(..) {
+        child.join();
+    }
     utxodb.flush()?;
-    wallet.apply_diff(&diff.0, &diff.1).unwrap();
     Ok(())
 }
