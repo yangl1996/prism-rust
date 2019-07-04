9cb47528e8e1237980bd91ec257af07871074920
wgr523 <wgr523@gmail.com>
2019-07-03 20:05:34 -0700

    clean code, set slow parameter

--- src/config.rs
+++ src/config.rs
@@ -4,9 +4,9 @@ use crate::crypto::hash::H256;
 pub const NETWORK_DELAY: f32 = 2.0; // the expected block propagation delay (in seconds)
 
 // Design parameters
-pub const NUM_VOTER_CHAINS: u16 = 10 as u16; // more chains means better latency
+pub const NUM_VOTER_CHAINS: u16 = 100 as u16; // more chains means better latency
 pub const TX_BLOCK_SIZE: u32 = 64_000; // the maximum size of a transaction block (in Bytes)
-pub const TX_THROUGHPUT: u32 = 70; // the transaction throughput we want to support (in Tx/s)
+pub const TX_THROUGHPUT: u32 = 500; // the transaction throughput we want to support (in Tx/s)
 pub const TX_BLOCK_TRANSACTIONS: u32 = TX_BLOCK_SIZE / AVG_TX_SIZE;
 
 pub const AVG_TX_SIZE: u32 = 280;   // average size of a transaction (in Bytes)
