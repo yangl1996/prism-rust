42631a13dcc559f289423602d2f8f3942dde3275
vishaalagartha <vishaalagartha@gmail.com>
2019-07-19 12:34:26 -0700

    persistent votes

--- visualization/longestChainWithVotes/vote.js
+++ visualization/longestChainWithVotes/vote.js
@@ -1,27 +1,30 @@
 let chainVotes = []
 const drawVotes = (voteData) => {
-  let vote = voteData.length===0 ? voteGroup.selectAll('.voteLink').data(chainVotes, d=>d.id) :
-                                   voteGroup.selectAll('.voteLink').data(voteData, d=>d.id)
+  let vote = voteData ? voteGroup.selectAll('.voteLink').data(voteData, d=>d.id) :
+                                   voteGroup.selectAll('.voteLink').data(chainVotes, d=>d.id)
 
   vote.exit().remove()
-  vote.enter().append('path')
-      .attr('id', d => d.id)
-      .attr('class', d => 'voteLink to' + d.to)
-      .attr('d', d=>{
-        return d.curve
-      })
-      .style('stroke-width', 3.0)
-      .attr('stroke', 'url(#linear-gradient)')
-      .style('filter', 'url(#blur)')
-      .attr('stroke-dasharray', d => d.totalLength + ' ' + d.totalLength)
-      .attr('stroke-dashoffset', d => d.totalLength)
-      .transition()
-      .duration(t)
-      .attr('stroke-dashoffset', 0)
-      .transition()
-      .duration(t)
-      .style('opacity', 0)
-      .remove()
+  let voteTransition = vote.enter().append('path')
+              .attr('id', d => d.id)
+              .attr('class', d => 'voteLink to' + d.to)
+              .attr('d', d=>{
+                return d.curve
+              })
+              .style('stroke-width', 3.0)
+              .attr('stroke', 'url(#linear-gradient)')
+              .style('filter', 'url(#blur)')
+              .attr('stroke-dasharray', d => d.totalLength + ' ' + d.totalLength)
+              .attr('stroke-dashoffset', d => d.totalLength)
+              .transition()
+              .duration(t)
+              .attr('stroke-dashoffset', 0)
+  if(voteData){
+    console.log('here')
+      voteTransition.transition()
+          .duration(t)
+          .style('opacity', 0)
+          .remove()
+  }
 }
 
 
@@ -68,7 +71,7 @@ const castVotes = (votingChain) => {
       const targetY = longestChainBlock.y + longestChainBlockSize/2 + longestChainBlockSize
 
       const data = `M${sourceX},${sourceY} Q${sourceX-50},${sourceY-50} ${targetX},${targetY}`
-      const voteObj = {from: lastBlock.blockId, to: longestChainBlock.blockId, fromChain: votingChain, id: 'vote'+lastBlock.blockId+'-'+longestChainBlock.blockId, curve: `M${sourceX},${sourceY} Q${sourceX-50},${sourceY-50} ${targetX},${targetY}`}
+      const voteObj = {from: lastBlock.blockId, to: longestChainBlock.blockId, fromChain: votingChain, id: 'vote'+lastBlock.blockId+'-'+longestChainBlock.id, curve: `M${sourceX},${sourceY} Q${sourceX-50},${sourceY-50} ${targetX},${targetY}`}
       let tempPath = voteGroup.append('path')
                               .attr('id', 'tempPath')
                               .attr('d', voteObj.curve)
@@ -78,8 +81,8 @@ const castVotes = (votingChain) => {
       chainsData[votingChain].lastVotedBlock = voteToCast
       voteToCast++
     }
-    drawVotes(chainVotes)
- }
+    drawVotes()
+  }
 }
 
 const mineVotingBlock = (votingChain, blockId, sourceNodeId, parentId, votes) => {
