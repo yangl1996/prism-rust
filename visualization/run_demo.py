707436a31a5d2aa5b77393dc0224650f59227177
vishaalagartha <vishaalagartha@gmail.com>
2019-08-12 13:14:01 -0700

    fixes run demo

--- visualization/run_demo.py
+++ visualization/run_demo.py
@@ -1,17 +1,5 @@
 import subprocess, signal, os
 import sys
-import keyboard
-sys.path.append('..')
-
-def print_pressed_keys(e):
-    key = list(keyboard._pressed_events.keys())
-    if len(key)>0 and key[0]==126:
-        os.system('killall LiveSlides')
-    if len(key)>0 and key[0]==125:
-        os.system('open -a LiveSlides')
-
-keyboard.hook(print_pressed_keys)
-keyboard.wait()
 
 def signal_handler(sig, frame):
     print('Killing all processes')
