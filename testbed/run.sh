eb3f55fdea2e830c43b9df7881fc7f2bf92014aa
Vishaal Agartha <vishaal@ip-172-31-2-128.us-west-1.compute.internal>
2019-08-12 19:15:08 +0000

    fixes stop-instances

--- testbed/run.sh
+++ testbed/run.sh
@@ -1,10 +1,68 @@
 #!/bin/bash
 
+DEFAULT_LAUNCH_TEMPLATE=lt-07c210f12b766840c
+DEFAULT_REGION='us-west-1'
+
+function start_instances
+{
+	# $1: number of instances to start
+	if [ $# -ne 1 ]; then
+		tput setaf 1
+		echo "Required: number of instances to start"
+		tput sgr0
+		exit 1
+	fi
+	echo "Really?"
+	select yn in "Yes" "No"; do
+		case $yn in
+			Yes ) break ;;
+			No ) echo "Nothing happened."; exit ;;
+		esac
+	done
+	echo "Launching $1 AWS EC2 instances"
+	local instances=`aws ec2 run-instances --launch-template LaunchTemplateId=$DEFAULT_LAUNCH_TEMPLATE --count $1 --query 'Instances[*].InstanceId' | jq -r '. | join(" ")'`
+	rm -f instances.txt
+	rm -f ~/.ssh/config.d/prism
+	echo "Querying public IPs and writing to SSH config"
+	while [ 1 ]
+	do
+		rawdetails=`aws ec2 describe-instances --instance-ids $instances --query 'Reservations[*].Instances[*].{publicip:PublicIpAddress,id:InstanceId,privateip:PrivateIpAddress}[]'`
+		if echo $rawdetails | jq '.[].publicip' | grep null &> /dev/null ; then
+			echo "Waiting for public IP addresses to be assigned"
+			sleep 3
+			continue
+		else
+			details=`echo "$rawdetails" | jq -c '.[]'`
+			break
+		fi
+	done
+	for instancedetail in $details;
+	do
+		local instance=`echo $instancedetail | jq -r '.id'`
+		local ip=`echo $instancedetail | jq -r '.publicip'`
+		local lan=`echo $instancedetail | jq -r '.privateip'`
+		echo "$instance,$ip,$lan,$DEFAULT_REGION" >> instances.txt
+		echo "Host $instance" >> ~/.ssh/config.d/prism
+		echo "    Hostname $ip" >> ~/.ssh/config.d/prism
+		echo "    User ubuntu" >> ~/.ssh/config.d/prism
+		echo "    IdentityFile ~/.ssh/prism.pem" >> ~/.ssh/config.d/prism
+		echo "    StrictHostKeyChecking no" >> ~/.ssh/config.d/prism
+		echo "    UserKnownHostsFile=/dev/null" >> ~/.ssh/config.d/prism
+		echo "" >> ~/.ssh/config.d/prism
+	done
+	echo "SSH config written, waiting for instances to initialize"
+	aws ec2 wait instance-running --instance-ids $instances
+	tput setaf 2
+	echo "Instances started"
+	tput sgr0
+	curl -s --form-string "token=$PUSHOVER_TOKEN" --form-string "user=$PUSHOVER_USER" --form-string "title=EC2 Instances Launched" --form-string "message=$1 EC2 instances were just launched by user $(whoami)." https://api.pushover.net/1/messages.json &> /dev/null
+}
+
 declare -a regions=("eu-north-1" "ap-south-1" "eu-west-3" "eu-west-2" "eu-west-1" "ap-northeast-2" "ap-northeast-1" "me-south-1" "ca-central-1" "ap-east-1" "ap-southeast-1" "ap-southeast-2" "eu-central-1" "us-east-1" "us-east-2" "us-west-1" "us-west-2")
 
 declare -a launch_template_ids=("lt-074165339867aa834" "lt-0e85ca27c485a13ce" "lt-0b77f96fe8631b010" "lt-0228fc0f23033acf5" "lt-00f97677e33e94706" "lt-0e380a9ccc0c4276a" "lt-0bd3ab2ea92f9c1f1" "lt-0d75559e93ea6b22d" "lt-094dfaed17f258c8d" "lt-0837a4136e9862849" "lt-0a154de3c17fb82ef" "lt-04153cb112577a813" "lt-009f97b35d4f1de3d" "lt-0d9bcde49095f337d" "lt-0e50969fb1afb62f2" "lt-07c210f12b766840c" "lt-0a7e1261dc0dca417")
 
-function start_instances
+function start_instances_global
 {
 	echo "Really?"
 	select yn in "Yes" "No"; do
@@ -37,11 +95,11 @@ function start_instances
 			tput sgr0
 			exit 1
 		fi
-		start_instances_single ${region} ${instances} ${launch_template_ids[$index]}
+		start_region_instances ${region} ${instances} ${launch_template_ids[$index]}
 	done < "$input"
 }
 
-function start_instances_single
+function start_region_instances
 {
 	echo "Launching AWS EC2 $2 instances at $1"
 	local instances=`aws ec2 run-instances --launch-template LaunchTemplateId=$3 --region $1 --count $2 --query 'Instances[*].InstanceId' | jq -r '. | join(" ")'`
@@ -554,6 +612,7 @@ case "$1" in
 		Manage AWS EC2 Instances
 
 		  start-instances n     Start n EC2 instances
+		  start-instances-global Start EC2 instances from ./config.txt
 		  stop-instances        Terminate EC2 instances
 		  install-tools         Install tools
 		  fix-config            Fix SSH config
@@ -594,6 +653,8 @@ case "$1" in
 		;;
 	start-instances)
 		start_instances $2 ;;
+	start-instances-global)
+		start_instances_global ;;
 	stop-instances)
 		stop_instances ;;
 	fix-config)
