1393be63ff200938d4dcc331b93ef82e19203635
Lei Yang <lei@yangl1996.com>
2019-08-28 18:12:40 -0400

    tune for perf

--- src/config.rs
+++ src/config.rs
@@ -6,15 +6,15 @@ pub const NETWORK_DELAY: f32 = 1.4; // the expected block propagation delay (in 
 // Design parameters
 pub const NUM_VOTER_CHAINS: u16 = 1000 as u16; // more chains means better latency
 pub const TX_BLOCK_SIZE: u32 = 64000; // the maximum size of a transaction block (in Bytes)
-pub const TX_THROUGHPUT: u32 = 70000; // the transaction throughput we want to support (in Tx/s)
+pub const TX_THROUGHPUT: u32 = 100000; // the transaction throughput we want to support (in Tx/s)
 pub const TX_BLOCK_TRANSACTIONS: u32 = TX_BLOCK_SIZE / AVG_TX_SIZE;
 pub const PROPOSER_BLOCK_TX_REFS: u32 = (TX_MINING_RATE / CHAIN_MINING_RATE * 2.0) as u32;
 
 pub const AVG_TX_SIZE: u32 = 280; // average size of a transaction (in Bytes)
 pub const TX_MINING_RATE: f32 = TX_THROUGHPUT as f32 / TX_BLOCK_TRANSACTIONS as f32;
-pub const CHAIN_MINING_RATE: f32 = 0.074; // mining rate of the proposer chain and each voter chain in Blks/s
+pub const CHAIN_MINING_RATE: f32 = 0.100; // mining rate of the proposer chain and each voter chain in Blks/s
 
-pub const ADVERSARY_MINING_POWER: f32 = 0.43; // the adversary power we want to tolerate
+pub const ADVERSARY_MINING_POWER: f32 = 0.40; // the adversary power we want to tolerate
 pub const LOG_EPSILON: f32 = 10.0; // -ln(1-confirmation_guarantee)
 pub const ALPHA: f32 = (CHAIN_MINING_RATE * NETWORK_DELAY) / (1.0 + CHAIN_MINING_RATE * NETWORK_DELAY); // alpha = orphan blocks / total blocks
 
