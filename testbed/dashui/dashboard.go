f143edf6be1e0f6c25e0ba975a1503234a418359
Lei Yang <lei@yangl1996.com>
2019-11-14 13:56:52 -0500

    prepare for more functionalities

---
+++ testbed/dashui/dashboard.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"log"
+	"os"
+	"time"
+	"flag"
+
+	"github.com/hajimehoshi/ebiten"
+)
+
+const w, h = 960, 540
+
+func dashboard(args []string) {
+	cmd := flag.NewFlagSet("dashboard", flag.ExitOnError)
+	logFlag := cmd.String("log", "../0.log", "Set the path to the Prism client log file")
+
+	cmd.Parse(args)
+
+	s := ebiten.DeviceScaleFactor()
+	ebiten.SetRunnableInBackground(true)
+
+	g := DefaultTimeSeries(w, h, s)
+	proposerSeries := TimeSeries{}
+	proposerSeries.Consolidation = Avg
+	proposerSeries.ConsolidationInterval = time.Duration(250) * time.Millisecond
+	voterSeries := TimeSeries{}
+	voterSeries.Consolidation = Avg
+	voterSeries.ConsolidationInterval = time.Duration(250) * time.Millisecond
+	transactionSeries := TimeSeries{}
+	transactionSeries.Consolidation = Avg
+	transactionSeries.ConsolidationInterval = time.Duration(250) * time.Millisecond
+	ds := []Dataset{&proposerSeries, &voterSeries, &transactionSeries}
+
+	m := g.PlotTimeSeries(ds, time.Now().Add(time.Duration(-60)*time.Second), time.Now())
+
+	go func() {
+		extractDelay(*logFlag, &proposerSeries, &voterSeries, &transactionSeries)
+	}()
+
+	go func() {
+		for range time.NewTicker(8 * time.Millisecond).C {
+			m = g.PlotTimeSeries(ds, time.Now().Add(time.Duration(-60)*time.Second), time.Now())
+		}
+	}()
+
+	update := func(screen *ebiten.Image) error {
+		if ebiten.IsKeyPressed(ebiten.KeyEscape) || ebiten.IsKeyPressed(ebiten.KeyQ) {
+			os.Exit(0)
+		}
+
+		if !ebiten.IsDrawingSkipped() {
+			screen.ReplacePixels(m.Pix)
+		}
+
+		return nil
+	}
+
+	if err := ebiten.Run(update, int(w*s), int(h*s), 1/s, "Prism"); err != nil {
+		log.Fatal(err)
+	}
+}
--- testbed/dashui/main.go
+++ testbed/dashui/main.go
@@ -1,56 +1,24 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"os"
-	"time"
-
-	"github.com/hajimehoshi/ebiten"
 )
 
-const w, h = 960, 540
-
 func main() {
-	s := ebiten.DeviceScaleFactor()
-	ebiten.SetRunnableInBackground(true)
-
-	g := DefaultTimeSeries(w, h, s)
-	proposerSeries := TimeSeries{}
-	proposerSeries.Consolidation = Avg
-	proposerSeries.ConsolidationInterval = time.Duration(250) * time.Millisecond
-	voterSeries := TimeSeries{}
-	voterSeries.Consolidation = Avg
-	voterSeries.ConsolidationInterval = time.Duration(250) * time.Millisecond
-	transactionSeries := TimeSeries{}
-	transactionSeries.Consolidation = Avg
-	transactionSeries.ConsolidationInterval = time.Duration(250) * time.Millisecond
-	ds := []Dataset{&proposerSeries, &voterSeries, &transactionSeries}
-
-	m := g.PlotTimeSeries(ds, time.Now().Add(time.Duration(-60)*time.Second), time.Now())
-
-	go func() {
-		extractDelay("../0.log", &proposerSeries, &voterSeries, &transactionSeries)
-	}()
-
-	go func() {
-		for range time.NewTicker(8 * time.Millisecond).C {
-			m = g.PlotTimeSeries(ds, time.Now().Add(time.Duration(-60)*time.Second), time.Now())
-		}
-	}()
-
-	update := func(screen *ebiten.Image) error {
-		if ebiten.IsKeyPressed(ebiten.KeyEscape) || ebiten.IsKeyPressed(ebiten.KeyQ) {
-			os.Exit(0)
-		}
-
-		if !ebiten.IsDrawingSkipped() {
-			screen.ReplacePixels(m.Pix)
-		}
-
-		return nil
+	if len(os.Args) < 2 {
+		usage()
 	}
 
-	if err := ebiten.Run(update, int(w*s), int(h*s), 1/s, "Ebiten + go-chart"); err != nil {
-		log.Fatal(err)
+	switch os.Args[1] {
+	case "dashboard":
+		dashboard(os.Args[2:])
+	default:
+		usage()
 	}
+}
+
+func usage() {
+	fmt.Println("Subcommands: dashboard")
+	os.Exit(1)
 }
