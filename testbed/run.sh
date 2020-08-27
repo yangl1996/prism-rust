7757c65def25af2005d6cd6b738a46018210ff36
Lei Yang <leiy@csail.mit.edu>
2020-08-27 19:31:07 +0000

    fig. 1 beta 20

--- testbed/run.sh
+++ testbed/run.sh
@@ -510,7 +510,7 @@ function start_transactions_single
 
 function start_mining_single
 {
-	curl -s "http://$3:$4/miner/start?lambda=322477&lazy=false"
+	curl -s "http://$3:$4/miner/start?lambda=220000&lazy=false"
 }
 
 function stop_transactions_single
