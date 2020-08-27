97d80d2776f601bc66748867030f07e7e0a53161
Lei Yang <leiy@csail.mit.edu>
2020-08-27 15:32:58 +0000

    use the new confirmation rule

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -691,9 +691,12 @@ impl BlockChain {
                 let voter_best = self.voter_best[*chain_num as usize].lock().unwrap();
                 let voter_best_level = voter_best.1;
                 drop(voter_best);
+                /*
                 total_vote_blocks += self
                     .num_voter_blocks(*chain_num, *vote_level, voter_best_level)
                     .unwrap();
+                    */
+                total_vote_blocks += voter_best_level - vote_level + 1;
                 total_vote_count += 1;
                 let this_depth = voter_best_level - vote_level + 1;
                 vote_depth.push(this_depth);
@@ -712,15 +715,6 @@ impl BlockChain {
 
         // no point in going further if less than 3/5 votes are cast
         if total_vote_count > self.config.voter_chains * 3 / 5 {
-            // calculate the average number of voter blocks mined after
-            // a vote is casted. we use this as an estimator of honest mining
-            // rate, and then derive the believed malicious mining rate
-            let avg_vote_blocks = total_vote_blocks as f32 / f32::from(total_vote_count);
-            // expected voter depth of an adversary
-            let adversary_expected_vote_depth =
-                avg_vote_blocks / (1.0 - self.config.adversary_ratio) * self.config.adversary_ratio;
-            let poisson = Poisson::new(f64::from(adversary_expected_vote_depth)).unwrap();
-
             // for each block calculate the lower bound on the number of votes
             let mut votes_lcb: HashMap<&H256, f32> = HashMap::new();
             let mut total_votes_lcb: f32 = 0.0;
@@ -729,71 +723,10 @@ impl BlockChain {
             for block in &proposer_blocks {
                 let votes = votes_depth.get(block).unwrap();
 
-                let mut block_votes_mean: f32 = 0.0; // mean E[X]
-                let mut block_votes_variance: f32 = 0.0; // Var[X]
-                let mut block_votes_lcb: f32 = 0.0;
-                for depth in votes.iter() {
-                    // probability that the adversary will remove this vote
-                    let mut p: f32 = 1.0 - poisson.cdf((*depth as f32 + 1.0).into()) as f32;
-                    for k in 0..(*depth as u64) {
-                        // probability that the adversary has mined k blocks
-                        let p1 = poisson.pmf(k) as f32;
-                        // probability that the adversary will overtake 'depth-k' blocks
-                        let p2 = (self.config.adversary_ratio
-                            / (1.0 - self.config.adversary_ratio))
-                            .powi((depth - k + 1) as i32);
-                        p += p1 * p2;
-                    }
-                    block_votes_mean += 1.0 - p;
-                    block_votes_variance += p * (1.0 - p);
-                }
-                // using gaussian approximation
-                let tmp = block_votes_mean
-                    - (block_votes_variance).sqrt() * quantile;
-                if tmp > 0.0 {
-                    block_votes_lcb += tmp;
-                }
-                votes_lcb.insert(block, block_votes_lcb);
-                total_votes_lcb += block_votes_lcb;
+                let cfm = self.config.try_confirm(total_vote_blocks, votes.len() as u64);
 
-                if max_vote_lcb < block_votes_lcb {
-                    max_vote_lcb = block_votes_lcb;
+                if cfm {
                     new_leader = Some(*block);
-                }
-                // In case of a tie, choose block with lower hash.
-                if (max_vote_lcb - block_votes_lcb).abs() < std::f32::EPSILON
-                    && new_leader.is_some()
-                {
-                    // TODO: is_some required?
-                    if *block < new_leader.unwrap() {
-                        new_leader = Some(*block);
-                    }
-                }
-            }
-            // check if the lcb_vote of new_leader is bigger than second best ucb votes
-            let remaining_votes = f32::from(self.config.voter_chains) - total_votes_lcb;
-
-            // if max_vote_lcb is lesser than the remaining_votes, then a private block could
-            // get the remaining votes and become the leader block
-            if max_vote_lcb <= remaining_votes || new_leader.is_none() {
-                new_leader = None;
-            } else {
-                for p_block in &proposer_blocks {
-                    // if the below condition is true, then final votes on p_block could overtake new_leader
-                    if max_vote_lcb < votes_lcb.get(p_block).unwrap() + remaining_votes
-                        && *p_block != new_leader.unwrap()
-                    {
-                        new_leader = None;
-                        break;
-                    }
-                    //In case of a tie, choose block with lower hash.
-                    if (max_vote_lcb - (votes_lcb.get(p_block).unwrap() + remaining_votes)).abs()
-                        < std::f32::EPSILON
-                        && *p_block < new_leader.unwrap()
-                    {
-                        new_leader = None;
-                        break;
-                    }
                 }
             }
         }
