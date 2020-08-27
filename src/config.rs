98e7772fcc635595ef890c3a7096e7ec28f9c697
Lei Yang <leiy@csail.mit.edu>
2020-08-27 17:20:11 +0000

    calculate less terms

--- src/config.rs
+++ src/config.rs
@@ -164,7 +164,7 @@ pub fn function_q(t_l: f32, lh_l: f32, la_l: f32) -> f32 {
     let lh = lh_l as f64;
     let la = la_l as f64;
 
-    let terms = 30;
+    let terms = 20;
 
     let mut res: f64 = 1.0;
     for l in 0..=terms {
